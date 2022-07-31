b37b5e6ecbb9176e3a56ddfdcf9925128d562960
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-07-31 23:26:36 +0900

    add files

---
+++ abc262/a/main.lisp
@@ -0,0 +1,131 @@
+(in-package :cl-user)
+
+(unless (member :child-sbcl *features*)
+  (quit
+   :recklessly-p t
+   :unix-status
+   (process-exit-code
+    (run-program *runtime-pathname*
+                 `("--control-stack-size" "256MB"
+                   "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
+                   "--eval" "(push :child-sbcl *features*)"
+                   "--script" ,(namestring *load-pathname*))
+                 :output t :error t :input t))))
+
+(macrolet ((def (b)
+             `(progn (deftype ,(intern (format nil "UINT~A" b)) () '(unsigned-byte ,b))
+                     (deftype ,(intern (format nil "INT~A" b)) () '(signed-byte ,b))))
+           (define-int-types (&rest bits) `(progn ,@(mapcar (lambda (b) `(def ,b)) bits))))
+  (define-int-types 2 4 7 8 15 16 31 32 62 63 64))
+
+(defconstant +mod+ 1000000007)
+
+(defmacro dbg (&rest forms)
+  (declare (ignorable forms))
+  (if (= (length forms) 1)
+      `(format t "~A => ~A~%" ',(car forms) ,(car forms))
+      `(format t "~A => ~A~%" ',forms `(,,@forms))))
+
+(defmethod print-object ((object hash-table) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream ":count ~A :test ~A"
+            (hash-table-count object)
+            (hash-table-test object))
+    (loop for k being the hash-key of object
+          do (format stream "~%[~A] ~A" k (gethash k object)))))
+
+(declaim (inline println printlns))
+(defun println (obj &optional (stream *standard-output*))
+  (let ((*read-default-float-format*
+          (if (typep obj 'double-float) 'double-float *read-default-float-format*)))
+    (prog1 (princ obj stream) (terpri stream))))
+
+(defun printlns (lst)
+  (format t "~{~A~%~}" lst))
+
+;; BEGIN_INSERTED_CONTENTS
+(defpackage :cp/read-fixnum
+  (:use :cl)
+  (:export #:read-fixnum))
+(in-package :cp/read-fixnum)
+
+(declaim (ftype (function * (values fixnum &optional)) read-fixnum))
+(defun read-fixnum (&optional (in *standard-input*))
+  "NOTE: cannot read -2^62"
+  (macrolet ((%read-byte ()
+               `(the (unsigned-byte 8)
+                     #+swank (char-code (read-char in nil #\Nul))
+                     #-swank (sb-impl::ansi-stream-read-byte in nil #.(char-code #\Nul) nil))))
+    (let* ((minus nil)
+           (result (loop (let ((byte (%read-byte)))
+                           (cond ((<= 48 byte 57)
+                                  (return (- byte 48)))
+                                 ((zerop byte) ; #\Nul
+                                  (error "Read EOF or #\Nul."))
+                                 ((= byte #.(char-code #\-))
+                                  (setq minus t)))))))
+      (declare ((integer 0 #.most-positive-fixnum) result))
+      (loop
+        (let* ((byte (%read-byte)))
+          (if (<= 48 byte 57)
+              (setq result (+ (- byte 48)
+                              (* 10 (the (integer 0 #.(floor most-positive-fixnum 10))
+                                         result))))
+              (return (if minus (- result) result))))))))
+
+;; BEGIN_USE_PACKAGE
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package :cp/read-fixnum :cl-user))
+
+;;;
+;;; Body
+;;;
+
+(in-package :cl-user)
+
+(defmacro readlet (vars &body body)
+  `(let ,(mapcar (lambda (v)
+                   `(,v (read-fixnum)))
+          vars)
+     ,@body))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun ensure-list (x)
+    (if (listp x)
+        x
+        (list x)))
+  (defun string-gensym (symbol)
+    (gensym (string symbol))))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/iter.lisp#L22
+(defmacro nlet (name bindings &body body)
+  (let* ((bindings (mapcar #'ensure-list bindings))
+         (vars  (mapcar #'first bindings))
+         (inits (mapcar #'second bindings))
+         (temps (mapcar #'string-gensym vars))
+         (gtag (string-gensym 'tag)))
+    `(block ,name
+       (let ,(mapcar #'list temps inits)
+         (macrolet ((,name ,vars
+                      `(progn
+                         (psetq
+                          ,@(mapcan #'list
+                                    ',temps
+                                    (list ,@vars)))
+                         (go ,',gtag))))
+           (tagbody
+              ,gtag (return-from ,name
+                      (let ,(mapcar #'list vars temps)
+                        ,@body))))))))
+
+(defun main ()
+  (readlet (y)
+    (println (let ((m (mod (- y 2) 4)))
+               (+ y
+                  (if (zerop m)
+                      0
+                      (- 4 m)))))))
+
+#-swank (main)
---
+++ abc262/b/main.lisp
@@ -0,0 +1,256 @@
+(in-package :cl-user)
+
+(unless (member :child-sbcl *features*)
+  (quit
+   :recklessly-p t
+   :unix-status
+   (process-exit-code
+    (run-program *runtime-pathname*
+                 `("--control-stack-size" "256MB"
+                   "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
+                   "--eval" "(push :child-sbcl *features*)"
+                   "--script" ,(namestring *load-pathname*))
+                 :output t :error t :input t))))
+
+(macrolet ((def (b)
+             `(progn (deftype ,(intern (format nil "UINT~A" b)) () '(unsigned-byte ,b))
+                     (deftype ,(intern (format nil "INT~A" b)) () '(signed-byte ,b))))
+           (define-int-types (&rest bits) `(progn ,@(mapcar (lambda (b) `(def ,b)) bits))))
+  (define-int-types 2 4 7 8 15 16 31 32 62 63 64))
+
+(defconstant +mod+ 1000000007)
+
+(defmacro dbg (&rest forms)
+  (declare (ignorable forms))
+  (if (= (length forms) 1)
+      `(format t "~A => ~A~%" ',(car forms) ,(car forms))
+      `(format t "~A => ~A~%" ',forms `(,,@forms))))
+
+(defmethod print-object ((object hash-table) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream ":count ~A :test ~A"
+            (hash-table-count object)
+            (hash-table-test object))
+    (loop for k being the hash-key of object
+          do (format stream "~%[~A] ~A" k (gethash k object)))))
+
+(declaim (inline println printlns))
+(defun println (obj &optional (stream *standard-output*))
+  (let ((*read-default-float-format*
+          (if (typep obj 'double-float) 'double-float *read-default-float-format*)))
+    (prog1 (princ obj stream) (terpri stream))))
+
+(defun printlns (lst)
+  (format t "~{~A~%~}" lst))
+
+;; BEGIN_INSERTED_CONTENTS
+(defpackage :cp/read-fixnum
+  (:use :cl)
+  (:export #:read-fixnum))
+(in-package :cp/read-fixnum)
+
+(declaim (ftype (function * (values fixnum &optional)) read-fixnum))
+(defun read-fixnum (&optional (in *standard-input*))
+  "NOTE: cannot read -2^62"
+  (macrolet ((%read-byte ()
+               `(the (unsigned-byte 8)
+                     #+swank (char-code (read-char in nil #\Nul))
+                     #-swank (sb-impl::ansi-stream-read-byte in nil #.(char-code #\Nul) nil))))
+    (let* ((minus nil)
+           (result (loop (let ((byte (%read-byte)))
+                           (cond ((<= 48 byte 57)
+                                  (return (- byte 48)))
+                                 ((zerop byte) ; #\Nul
+                                  (error "Read EOF or #\Nul."))
+                                 ((= byte #.(char-code #\-))
+                                  (setq minus t)))))))
+      (declare ((integer 0 #.most-positive-fixnum) result))
+      (loop
+        (let* ((byte (%read-byte)))
+          (if (<= 48 byte 57)
+              (setq result (+ (- byte 48)
+                              (* 10 (the (integer 0 #.(floor most-positive-fixnum 10))
+                                         result))))
+              (return (if minus (- result) result))))))))
+
+(defpackage :cp/disjoint-set
+  (:use :cl)
+  (:export #:disjoint-set #:make-disjoint-set #:ds-data
+           #:ds-root #:ds-unite! #:ds-connected-p #:ds-size #:ds-clear)
+  (:documentation "Provides disjoint set implementation with union by size and
+path compression."))
+(in-package :cp/disjoint-set)
+
+(defstruct (disjoint-set
+            (:constructor make-disjoint-set
+                (size &aux (data (make-array size :element-type 'fixnum :initial-element -1))))
+            (:conc-name ds-)
+            (:predicate nil)
+            (:copier nil))
+  (data nil :type (simple-array fixnum (*))))
+
+(declaim (inline ds-root))
+(defun ds-root (disjoint-set x)
+  "Returns the root of X."
+  (declare ((mod #.array-dimension-limit) x))
+  (let ((data (ds-data disjoint-set)))
+    (labels ((recur (x)
+               (if (< (aref data x) 0)
+                   x
+                   (setf (aref data x)
+                         (recur (aref data x))))))
+      (recur x))))
+
+(declaim (inline ds-unite!))
+(defun ds-unite! (disjoint-set x1 x2)
+  "Destructively unites X1 and X2 and returns true iff X1 and X2 become
+connected for the first time."
+  (let ((root1 (ds-root disjoint-set x1))
+        (root2 (ds-root disjoint-set x2)))
+    (unless (= root1 root2)
+      (let ((data (ds-data disjoint-set)))
+        ;; NOTE: If you want X1 to always be root, just delete this form. (Time
+        ;; complexity becomes worse, however.)
+        (when (> (aref data root1) (aref data root2))
+          (rotatef root1 root2))
+        (incf (aref data root1) (aref data root2))
+        (setf (aref data root2) root1)))))
+
+(declaim (inline ds-connected-p))
+(defun ds-connected-p (disjoint-set x1 x2)
+  "Returns true iff X1 and X2 have the same root."
+  (= (ds-root disjoint-set x1) (ds-root disjoint-set x2)))
+
+(declaim (inline ds-size))
+(defun ds-size (disjoint-set x)
+  "Returns the size of the connected component to which X belongs."
+  (- (aref (ds-data disjoint-set)
+           (ds-root disjoint-set x))))
+
+(declaim (inline ds-clear))
+(defun ds-clear (disjoint-set)
+  "Deletes all connections."
+  (fill (ds-data disjoint-set) -1)
+  disjoint-set)
+
+;; BEGIN_USE_PACKAGE
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package :cp/read-fixnum :cl-user))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package :cp/disjoint-set :cl-user))
+
+;;;
+;;; Body
+;;;
+
+(in-package :cl-user)
+
+(defmacro readlet (vars &body body)
+  `(let ,(mapcar (lambda (v)
+                   `(,v (read-fixnum)))
+          vars)
+     ,@body))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun ensure-list (x)
+    (if (listp x)
+        x
+        (list x)))
+  (defun string-gensym (symbol)
+    (gensym (string symbol))))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/iter.lisp#L22
+(defmacro nlet (name bindings &body body)
+  (let* ((bindings (mapcar #'ensure-list bindings))
+         (vars  (mapcar #'first bindings))
+         (inits (mapcar #'second bindings))
+         (temps (mapcar #'string-gensym vars))
+         (gtag (string-gensym 'tag)))
+    `(block ,name
+       (let ,(mapcar #'list temps inits)
+         (macrolet ((,name ,vars
+                      `(progn
+                         (psetq
+                          ,@(mapcan #'list
+                                    ',temps
+                                    (list ,@vars)))
+                         (go ,',gtag))))
+           (tagbody
+              ,gtag (return-from ,name
+                      (let ,(mapcar #'list vars temps)
+                        ,@body))))))))
+
+(defmacro collect (n expr)
+  `(loop repeat ,n
+         collect ,expr))
+
+(defmacro define-accumulations ()
+  `(progn
+     ,@(loop for acc in '(sum maximize minimize thereis always)
+             for fn in '(+ max min or and)
+             collect
+             `(defmacro ,acc ((var &rest args) &body body)
+                (ecase (length args)
+                  (1 `(reduce ',',fn ,(car args)
+                              :key (lambda (,var) ,@body)))
+                  (2 `(loop for ,var from ,(first args) below ,(second args)
+                            ,',acc (progn ,@body)))))
+             collect
+             `(defmacro ,(intern (format nil "~A*" acc)) (var-and-args-specs &body body)
+                (labels ((rec (specs)
+                           (if (null specs)
+                               `(progn ,@body)
+                               `(,',acc ,(car specs)
+                                        ,(rec (cdr specs))))))
+                  (rec var-and-args-specs))))))
+
+(define-accumulations)
+
+(defmacro counting ((var &rest args) &body body)
+  (ecase (length args)
+    (1 `(reduce (lambda (acc ,var) (+ acc (if (progn ,@body) 1 0)))
+                ,(car args) :initial-value 0))
+    (2 `(loop for ,var from ,(first args) below ,(second args)
+              count (progn ,@body)))))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun singletonp (list)
+    (and (consp list) (null (cdr list)))))
+
+(defmacro counting* (var-and-args-specs &body body)
+  (if (null var-and-args-specs)
+      0
+      (labels ((rec (specs)
+                 (if (singletonp specs)
+                     `(counting ,(car specs)
+                                (progn ,@body))
+                     `(sum ,(car specs)
+                           ,(rec (cdr specs))))))
+        (rec var-and-args-specs))))
+
+(defun make-bit-array (dimensions)
+  (make-array dimensions :element-type 'bit :initial-element 0))
+
+(defun solve (n m es)
+  (declare (ignore n))
+  (let ((adj (make-bit-array `(,(1+ n) ,(1+ n)))))
+    (loop for (u . v) in es do
+      (setf (aref adj u v) 1
+            (aref adj v u) 1))
+    (counting* ((a 1 (1- n))
+                (b (1+ a) n)
+                (c (1+ b) (1+ n)))
+      (plusp (* (aref adj a b)
+                (aref adj b c)
+                (aref adj c a))))))
+
+(defun main ()
+  (readlet (n m)
+    (let ((es (collect m
+                (readlet (u v) (cons u v)))))
+      (println (solve n m es)))))
+
+#-swank (main)
