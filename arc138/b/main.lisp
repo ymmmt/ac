4f241c3f66dcbf12a42b69c4a44406176132604d
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-08-04 22:56:39 +0900

    add file

---
+++ arc138/b/main.lisp
@@ -0,0 +1,170 @@
+(in-package :cl-user)
+
+(unless (member :child-sbcl *features*)
+  (quit
+   :recklessly-p t
+   :unix-status
+   (process-exit-code
+    (run-program *runtime-pathname*
+                 `("--control-stack-size" "256MB"
+                   "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
+                   "--eval" "(push :child-sbcl *features*)"
+                   "--script" ,(namestring *load-pathname*))
+                 :output t :error t :input t))))
+
+(macrolet ((def (b)
+             `(progn (deftype ,(intern (format nil "UINT~A" b)) () '(unsigned-byte ,b))
+                     (deftype ,(intern (format nil "INT~A" b)) () '(signed-byte ,b))))
+           (define-int-types (&rest bits) `(progn ,@(mapcar (lambda (b) `(def ,b)) bits))))
+  (define-int-types 2 4 7 8 15 16 31 32 62 63 64))
+
+(defconstant +mod+ 1000000007)
+
+(defmacro dbg (&rest forms)
+  (declare (ignorable forms))
+  (if (= (length forms) 1)
+      `(format t "~A => ~A~%" ',(car forms) ,(car forms))
+      `(format t "~A => ~A~%" ',forms `(,,@forms))))
+
+(defmethod print-object ((object hash-table) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream ":count ~A :test ~A"
+            (hash-table-count object)
+            (hash-table-test object))
+    (loop for k being the hash-key of object
+          do (format stream "~%[~A] ~A" k (gethash k object)))))
+
+(declaim (inline println printlns))
+(defun println (obj &optional (stream *standard-output*))
+  (let ((*read-default-float-format*
+          (if (typep obj 'double-float) 'double-float *read-default-float-format*)))
+    (prog1 (princ obj stream) (terpri stream))))
+
+(defun printlns (lst)
+  (format t "~{~A~%~}" lst))
+
+;; BEGIN_INSERTED_CONTENTS
+(defpackage :cp/read-fixnum
+  (:use :cl)
+  (:export #:read-fixnum))
+(in-package :cp/read-fixnum)
+
+(declaim (ftype (function * (values fixnum &optional)) read-fixnum))
+(defun read-fixnum (&optional (in *standard-input*))
+  "NOTE: cannot read -2^62"
+  (macrolet ((%read-byte ()
+               `(the (unsigned-byte 8)
+                     #+swank (char-code (read-char in nil #\Nul))
+                     #-swank (sb-impl::ansi-stream-read-byte in nil #.(char-code #\Nul) nil))))
+    (let* ((minus nil)
+           (result (loop (let ((byte (%read-byte)))
+                           (cond ((<= 48 byte 57)
+                                  (return (- byte 48)))
+                                 ((zerop byte) ; #\Nul
+                                  (error "Read EOF or #\Nul."))
+                                 ((= byte #.(char-code #\-))
+                                  (setq minus t)))))))
+      (declare ((integer 0 #.most-positive-fixnum) result))
+      (loop
+        (let* ((byte (%read-byte)))
+          (if (<= 48 byte 57)
+              (setq result (+ (- byte 48)
+                              (* 10 (the (integer 0 #.(floor most-positive-fixnum 10))
+                                         result))))
+              (return (if minus (- result) result))))))))
+
+;; BEGIN_USE_PACKAGE
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package :cp/read-fixnum :cl-user))
+
+;;;
+;;; Body
+;;;
+
+(in-package :cl-user)
+
+(defmacro readlet (vars &body body)
+  `(let ,(mapcar (lambda (v)
+                   `(,v (read-fixnum)))
+          vars)
+     ,@body))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun ensure-list (x)
+    (if (listp x)
+        x
+        (list x)))
+  (defun string-gensym (symbol)
+    (gensym (string symbol))))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/iter.lisp#L22
+(defmacro nlet (name bindings &body body)
+  (let* ((bindings (mapcar #'ensure-list bindings))
+         (vars  (mapcar #'first bindings))
+         (inits (mapcar #'second bindings))
+         (temps (mapcar #'string-gensym vars))
+         (gtag (string-gensym 'tag)))
+    `(block ,name
+       (let ,(mapcar #'list temps inits)
+         (macrolet ((,name ,vars
+                      `(progn
+                         (psetq
+                          ,@(mapcan #'list
+                                    ',temps
+                                    (list ,@vars)))
+                         (go ,',gtag))))
+           (tagbody
+              ,gtag (return-from ,name
+                      (let ,(mapcar #'list vars temps)
+                        ,@body))))))))
+
+(defun readvector (&rest args)
+  (values (read-from-string
+           (concatenate 'string "#("
+                        (apply #'read-line args)
+                        ")"))))
+
+;; 操作Aの逆操作: 先頭の0をひとつ除いて残りをflipする
+;; 操作Bの逆操作: 末尾の0をひとつ除く
+;; この2つの逆操作の有限回の適用によってaを空列にできることは
+;; 答えがYesであることの必要十分条件。
+
+;; 次の貪欲法によって探索範囲[l,r]の長さを0にできれば
+;; aを空列にできる。
+;; (1) 部分列[l,r]の先頭の0を1つを取り除く。これはAの逆操作にあたる。
+;; この操作に失敗してもいいのは、部分列がすべて0である場合(成功)のみで、
+;; それ以外の場合に失敗すれば答えはNo。
+;; (2) 部分列[l,r]の末尾の0個以上の0をすべて取り除く。これはBの逆操作にあたる。
+;; (1)と(2)の適用結果で再帰する。
+;; ビットのフリップを毎回するのは非効率なので
+;; その代りに再帰ごとに0と1の扱いをフリップする。
+(defun solve (n a)
+  (labels ((flip (bit)
+             (logxor bit 1))
+           (rec (bit l r)
+             (assert (<= l r))
+             (cond ((< l r)
+                    (or (loop for i from l to r
+                              always (= bit (svref a i)))
+                        (and (= bit (svref a l))
+                             (let ((i (position (flip bit) a
+                                                :start l :end (1+ r)))
+                                   (j (position (flip bit) a
+                                                :from-end t
+                                                :start l :end (1+ r))))
+                               (and (= (1+ l) i)
+                                    (rec (flip bit) i j))))))
+                   ((= l r)
+                    (= (svref a l) bit)))))
+    (rec 0 0 (1- n))))
+                  
+(defun main ()
+  (readlet (n)
+    (let ((a (readvector)))
+      (println (if (solve n a)
+                   "Yes"
+                   "No")))))
+
+#-swank (main)
