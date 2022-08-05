5498f7c9308c2849e0af97976a4ab513f4b62a92
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-08-06 03:43:26 +0900

    add files

---
+++ abc076/c/main.lisp
@@ -0,0 +1,194 @@
+(in-package :cl-user)
+
+(unless (member :child-sbcl *features*)
+  (quit
+   :recklessly-p t
+   :unix-status
+   (process-exit-code
+    (run-program *runtime-pathname*
+                 `("--control-stack-size" "256MB"
+                   "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
+                   "--eval" "(push :child-sbcl *features*)"
+                   "--script" ,(namestring *load-pathname*))
+                 :output t :error t :input t))))
+
+(macrolet ((def (b)
+             `(progn (deftype ,(intern (format nil "UINT~A" b)) () '(unsigned-byte ,b))
+                     (deftype ,(intern (format nil "INT~A" b)) () '(signed-byte ,b))))
+           (define-int-types (&rest bits) `(progn ,@(mapcar (lambda (b) `(def ,b)) bits))))
+  (define-int-types 2 4 7 8 15 16 31 32 62 63 64))
+
+(defconstant +mod+ 1000000007)
+
+(defmacro dbg (&rest forms)
+  (declare (ignorable forms))
+  (if (= (length forms) 1)
+      `(format t "~A => ~A~%" ',(car forms) ,(car forms))
+      `(format t "~A => ~A~%" ',forms `(,,@forms))))
+
+(defmethod print-object ((object hash-table) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream ":count ~A :test ~A"
+            (hash-table-count object)
+            (hash-table-test object))
+    (loop for k being the hash-key of object
+          do (format stream "~%[~A] ~A" k (gethash k object)))))
+
+(declaim (inline println printlns))
+(defun println (obj &optional (stream *standard-output*))
+  (let ((*read-default-float-format*
+          (if (typep obj 'double-float) 'double-float *read-default-float-format*)))
+    (prog1 (princ obj stream) (terpri stream))))
+
+(defun printlns (lst)
+  (format t "~{~A~%~}" lst))
+
+;; BEGIN_INSERTED_CONTENTS
+(defpackage :cp/read-fixnum
+  (:use :cl)
+  (:export #:read-fixnum))
+(in-package :cp/read-fixnum)
+
+(declaim (ftype (function * (values fixnum &optional)) read-fixnum))
+(defun read-fixnum (&optional (in *standard-input*))
+  "NOTE: cannot read -2^62"
+  (macrolet ((%read-byte ()
+               `(the (unsigned-byte 8)
+                     #+swank (char-code (read-char in nil #\Nul))
+                     #-swank (sb-impl::ansi-stream-read-byte in nil #.(char-code #\Nul) nil))))
+    (let* ((minus nil)
+           (result (loop (let ((byte (%read-byte)))
+                           (cond ((<= 48 byte 57)
+                                  (return (- byte 48)))
+                                 ((zerop byte) ; #\Nul
+                                  (error "Read EOF or #\Nul."))
+                                 ((= byte #.(char-code #\-))
+                                  (setq minus t)))))))
+      (declare ((integer 0 #.most-positive-fixnum) result))
+      (loop
+        (let* ((byte (%read-byte)))
+          (if (<= 48 byte 57)
+              (setq result (+ (- byte 48)
+                              (* 10 (the (integer 0 #.(floor most-positive-fixnum 10))
+                                         result))))
+              (return (if minus (- result) result))))))))
+
+;; BEGIN_USE_PACKAGE
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package :cp/read-fixnum :cl-user))
+
+;;;
+;;; Body
+;;;
+
+(in-package :cl-user)
+
+(defmacro readlet (vars &body body)
+  `(let ,(mapcar (lambda (v)
+                   `(,v (read-fixnum)))
+          vars)
+     ,@body))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun ensure-list (x)
+    (if (listp x)
+        x
+        (list x)))
+  (defun string-gensym (symbol)
+    (gensym (string symbol))))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/iter.lisp#L22
+(defmacro nlet (name bindings &body body)
+  (let* ((bindings (mapcar #'ensure-list bindings))
+         (vars  (mapcar #'first bindings))
+         (inits (mapcar #'second bindings))
+         (temps (mapcar #'string-gensym vars))
+         (gtag (string-gensym 'tag)))
+    `(block ,name
+       (let ,(mapcar #'list temps inits)
+         (macrolet ((,name ,vars
+                      `(progn
+                         (psetq
+                          ,@(mapcan #'list
+                                    ',temps
+                                    (list ,@vars)))
+                         (go ,',gtag))))
+           (tagbody
+              ,gtag (return-from ,name
+                      (let ,(mapcar #'list vars temps)
+                        ,@body))))))))
+
+(defun curry (function &rest arguments)
+  (lambda (&rest args)
+    (multiple-value-call function
+      (values-list arguments)
+      (values-list args))))
+
+(defmacro -> (x &rest forms)
+  (if (first forms)
+      (let* ((form (first forms))
+             (threaded (if (listp form)
+                           (if (member '% form)
+                               `(funcall (lambda (%) ,form) ,x)
+                               `(,(first form) ,x ,@(rest form)))
+                           `(,form ,x))))
+        `(-> ,threaded ,@(rest forms)))
+      x))
+
+(defun range (&rest args)
+  (ecase (length args)
+    (1 (let ((end (car args)))
+         (loop for i below end
+               collect i)))
+    (2 (destructuring-bind (start end) args
+         (loop for i from start below end
+               collect i)))
+    (3 (destructuring-bind (start end step) args
+         (loop for i from start below end by step
+               collect i)))))
+
+(defun filter-map (f list &rest more-lists)
+  (labels ((rec (list more-lists acc)
+             (if (and (consp list) (every #'consp more-lists))
+                 (rec (cdr list)
+                      (mapcar #'cdr more-lists)
+                      (let ((value (apply f (car list) (mapcar #'car more-lists))))
+                        (if value
+                            (cons value acc)
+                            acc)))
+                 (nreverse acc))))
+    (rec list more-lists nil)))
+
+(defun match (s u m i)
+  (let ((res (copy-seq s)))
+    (when (loop for di below m
+                always (or (char= (char s (+ i di)) #\?)
+                           (char= (char s (+ i di))
+                                  (char u di))))
+      (dotimes (di m)
+        (setf (char res (+ i di))
+              (char u di)))
+      res)))
+
+(defun instantiate (string)
+  (substitute #\a #\? string))
+
+(defun solve (s u)
+  (let ((n (length s))
+        (m (length u)))
+    (when (>= n m)
+      (-> (filter-map (curry #'match s u m)
+                      (range (1+ (- n m))))
+          (mapcar #'instantiate %)
+          (sort #'string<)
+          car))))
+                
+(defun main ()
+  (let ((s (read-line))
+        (u (read-line)))
+    (println (or (solve s u)
+                 "UNRESTORABLE"))))
+
+#-swank (main)
