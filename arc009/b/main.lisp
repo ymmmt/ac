451d9246a8db8049df4ec00efd434cf9f5ae506b
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-08-08 07:28:50 +0900

    add file

---
+++ arc009/b/main.lisp
@@ -0,0 +1,178 @@
+(in-package :cl-user)
+
+(unless (member :child-sbcl *features*)
+  (quit
+   :recklessly-p t
+   :unix-status
+   (process-exit-code
+    (run-program *runtime-pathname*
+                 `("--control-stack-size" "256MB"
+                   "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
+                   "--eval" "(push :child-sbcl *features*)"
+                   "--script" ,(namestring *load-pathname*))
+                 :output t :error t :input t))))
+
+(macrolet ((def (b)
+             `(progn (deftype ,(intern (format nil "UINT~A" b)) () '(unsigned-byte ,b))
+                     (deftype ,(intern (format nil "INT~A" b)) () '(signed-byte ,b))))
+           (define-int-types (&rest bits) `(progn ,@(mapcar (lambda (b) `(def ,b)) bits))))
+  (define-int-types 2 4 7 8 15 16 31 32 62 63 64))
+
+(defconstant +mod+ 1000000007)
+
+(defmacro dbg (&rest forms)
+  (declare (ignorable forms))
+  (if (= (length forms) 1)
+      `(format t "~A => ~A~%" ',(car forms) ,(car forms))
+      `(format t "~A => ~A~%" ',forms `(,,@forms))))
+
+(defmethod print-object ((object hash-table) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream ":count ~A :test ~A"
+            (hash-table-count object)
+            (hash-table-test object))
+    (loop for k being the hash-key of object
+          do (format stream "~%[~A] ~A" k (gethash k object)))))
+
+(declaim (inline println printlns))
+(defun println (obj &optional (stream *standard-output*))
+  (let ((*read-default-float-format*
+          (if (typep obj 'double-float) 'double-float *read-default-float-format*)))
+    (prog1 (princ obj stream) (terpri stream))))
+
+(defun printlns (lst)
+  (format t "~{~A~%~}" lst))
+
+;; BEGIN_INSERTED_CONTENTS
+(defpackage :cp/read-fixnum
+  (:use :cl)
+  (:export #:read-fixnum))
+(in-package :cp/read-fixnum)
+
+(declaim (ftype (function * (values fixnum &optional)) read-fixnum))
+(defun read-fixnum (&optional (in *standard-input*))
+  "NOTE: cannot read -2^62"
+  (macrolet ((%read-byte ()
+               `(the (unsigned-byte 8)
+                     #+swank (char-code (read-char in nil #\Nul))
+                     #-swank (sb-impl::ansi-stream-read-byte in nil #.(char-code #\Nul) nil))))
+    (let* ((minus nil)
+           (result (loop (let ((byte (%read-byte)))
+                           (cond ((<= 48 byte 57)
+                                  (return (- byte 48)))
+                                 ((zerop byte) ; #\Nul
+                                  (error "Read EOF or #\Nul."))
+                                 ((= byte #.(char-code #\-))
+                                  (setq minus t)))))))
+      (declare ((integer 0 #.most-positive-fixnum) result))
+      (loop
+        (let* ((byte (%read-byte)))
+          (if (<= 48 byte 57)
+              (setq result (+ (- byte 48)
+                              (* 10 (the (integer 0 #.(floor most-positive-fixnum 10))
+                                         result))))
+              (return (if minus (- result) result))))))))
+
+;; BEGIN_USE_PACKAGE
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package :cp/read-fixnum :cl-user))
+
+;;
+;; Utility
+;;
+
+(in-package :cl-user)
+
+(defmacro readlet (vars &body body)
+  `(let ,(mapcar (lambda (v)
+                   `(,v (read-fixnum)))
+          vars)
+     ,@body))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun ensure-list (x)
+    (if (listp x)
+        x
+        (list x)))
+  (defun string-gensym (symbol)
+    (gensym (string symbol))))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/iter.lisp#L22
+(defmacro nlet (name bindings &body body)
+  (let* ((bindings (mapcar #'ensure-list bindings))
+         (vars  (mapcar #'first bindings))
+         (inits (mapcar #'second bindings))
+         (temps (mapcar #'string-gensym vars))
+         (gtag (string-gensym 'tag)))
+    `(block ,name
+       (let ,(mapcar #'list temps inits)
+         (macrolet ((,name ,vars
+                      `(progn
+                         (psetq
+                          ,@(mapcan #'list
+                                    ',temps
+                                    (list ,@vars)))
+                         (go ,',gtag))))
+           (tagbody
+              ,gtag (return-from ,name
+                      (let ,(mapcar #'list vars temps)
+                        ,@body))))))))
+
+(defmacro dlambda (lambda-list &body body)
+  (let ((gargs (gensym "ARGS")))
+    `(lambda (&rest ,gargs)
+       (destructuring-bind ,lambda-list ,gargs
+         ,@body))))
+
+(defun readlist (&rest args)
+  (values (read-from-string
+           (concatenate 'string "("
+                        (apply #'read-line args)
+                        ")"))))
+
+(defmacro collect (n expr)
+  `(loop repeat ,n
+         collect ,expr))
+
+(defun num->digits (num &optional (base 10))
+  (assert (plusp num))
+  (nlet rec ((num num) (acc nil) (length 0))
+    (if (zerop num)
+        (values acc length)
+        (rec (floor num base)
+             (cons (mod num base) acc)
+             (1+ length)))))
+
+(defun digits->num (digits &optional (base 10))
+  (reduce (lambda (acc d) (+ (* acc base) d))
+          digits))
+
+(defun mapper (hash-table &optional default)
+  (lambda (key)
+    (gethash key hash-table default)))
+
+;;;
+;;; Body
+;;;
+
+(defun solve (b n a)
+  (declare (ignore n))
+  (let ((digit-map (make-hash-table)))
+    (nlet rec ((i 0) (b b))
+      (when b
+        (setf (gethash (car b) digit-map) i)
+        (rec (1+ i) (cdr b))))
+    (sort a #'<
+          :key (lambda (ai)
+                 (digits->num (mapcar (mapper digit-map)
+                                      (num->digits ai)))))))
+                       
+(defun main ()
+  (let ((b (readlist)))
+    (readlet (n)
+      (let ((a (collect n (read-fixnum))))
+        (printlns (solve b n a))))))
+
+#-swank (main)
