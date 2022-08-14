297bf802d9ad94f57520d5f438648d81f018b926
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-08-14 14:10:38 +0900

    add file

---
+++ arc119/b/main.lisp
@@ -0,0 +1,186 @@
+(in-package :cl-user)
+
+(unless (member :child-sbcl *features*)
+  (quit
+   :recklessly-p t
+   :unix-status
+   (process-exit-code
+    (run-program *runtime-pathname*
+                 `("--control-stack-size" "256MB"
+                   "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
+                   "--eval" "(push :child-sbcl *features*)"
+                   "--script" ,(namestring *load-pathname*))
+                 :output t :error t :input t))))
+
+(macrolet ((def (b)
+             `(progn (deftype ,(intern (format nil "UINT~A" b)) () '(unsigned-byte ,b))
+                     (deftype ,(intern (format nil "INT~A" b)) () '(signed-byte ,b))))
+           (define-int-types (&rest bits) `(progn ,@(mapcar (lambda (b) `(def ,b)) bits))))
+  (define-int-types 2 4 7 8 15 16 31 32 62 63 64))
+
+(defconstant +mod+ 1000000007)
+
+(defmacro dbg (&rest forms)
+  (declare (ignorable forms))
+  (if (= (length forms) 1)
+      `(format t "~A => ~A~%" ',(car forms) ,(car forms))
+      `(format t "~A => ~A~%" ',forms `(,,@forms))))
+
+(defmethod print-object ((object hash-table) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream ":count ~A :test ~A"
+            (hash-table-count object)
+            (hash-table-test object))
+    (loop for k being the hash-key of object
+          do (format stream "~%[~A] ~A" k (gethash k object)))))
+
+(declaim (inline println printlns))
+(defun println (obj &optional (stream *standard-output*))
+  (let ((*read-default-float-format*
+          (if (typep obj 'double-float) 'double-float *read-default-float-format*)))
+    (prog1 (princ obj stream) (terpri stream))))
+
+(defun printlns (lst)
+  (format t "~{~A~%~}" lst))
+
+;; BEGIN_INSERTED_CONTENTS
+(defpackage :cp/read-fixnum
+  (:use :cl)
+  (:export #:read-fixnum))
+(in-package :cp/read-fixnum)
+
+(declaim (ftype (function * (values fixnum &optional)) read-fixnum))
+(defun read-fixnum (&optional (in *standard-input*))
+  "NOTE: cannot read -2^62"
+  (macrolet ((%read-byte ()
+               `(the (unsigned-byte 8)
+                     #+swank (char-code (read-char in nil #\Nul))
+                     #-swank (sb-impl::ansi-stream-read-byte in nil #.(char-code #\Nul) nil))))
+    (let* ((minus nil)
+           (result (loop (let ((byte (%read-byte)))
+                           (cond ((<= 48 byte 57)
+                                  (return (- byte 48)))
+                                 ((zerop byte) ; #\Nul
+                                  (error "Read EOF or #\Nul."))
+                                 ((= byte #.(char-code #\-))
+                                  (setq minus t)))))))
+      (declare ((integer 0 #.most-positive-fixnum) result))
+      (loop
+        (let* ((byte (%read-byte)))
+          (if (<= 48 byte 57)
+              (setq result (+ (- byte 48)
+                              (* 10 (the (integer 0 #.(floor most-positive-fixnum 10))
+                                         result))))
+              (return (if minus (- result) result))))))))
+
+;; BEGIN_USE_PACKAGE
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package :cp/read-fixnum :cl-user))
+
+;;
+;; Utility
+;;
+
+(in-package :cl-user)
+
+(defmacro readlet (vars &body body)
+  `(let ,(mapcar (lambda (v)
+                   `(,v (read-fixnum)))
+          vars)
+     ,@body))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun ensure-list (x)
+    (if (listp x)
+        x
+        (list x)))
+  (defun string-gensym (symbol)
+    (gensym (string symbol))))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/definitions.lisp#L129
+(defmacro defsubst (name params &body body)
+  `(progn
+     (declaim (inline ,name))
+     (defun ,name ,params
+       ,@body)))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/iter.lisp#L22
+(defmacro nlet (name bindings &body body)
+  (let* ((bindings (mapcar #'ensure-list bindings))
+         (vars  (mapcar #'first bindings))
+         (inits (mapcar #'second bindings))
+         (temps (mapcar #'string-gensym vars))
+         (gtag (string-gensym 'tag)))
+    `(block ,name
+       (let ,(mapcar #'list temps inits)
+         (macrolet ((,name ,vars
+                      `(progn
+                         (psetq
+                          ,@(mapcan #'list
+                                    ',temps
+                                    (list ,@vars)))
+                         (go ,',gtag))))
+           (tagbody
+              ,gtag (return-from ,name
+                      (let ,(mapcar #'list vars temps)
+                        ,@body))))))))
+
+(defmacro dlambda (lambda-list &body body)
+  (let ((gargs (gensym "ARGS")))
+    `(lambda (&rest ,gargs)
+       (destructuring-bind ,lambda-list ,gargs
+         ,@body))))
+
+(defun position-all (item list &key (start 0) end key test)
+  (setf list (subseq list start end))
+  (labels ((rec (offset rest acc)
+             (let ((i (position item rest :key key :test test)))
+               (if i
+                   (rec (+ offset i 1)
+                        (nthcdr (1+ i) rest)
+                        (cons (+ i offset) acc))
+                   (nreverse acc)))))
+    (rec start list nil)))
+
+(defun filter-map (f list &rest more-lists)
+  (labels ((rec (list more-lists acc)
+             (if (and (consp list) (every #'consp more-lists))
+                 (rec (cdr list)
+                      (mapcar #'cdr more-lists)
+                      (let ((value (apply f (car list) (mapcar #'car more-lists))))
+                        (if value
+                            (cons value acc)
+                            acc)))
+                 (nreverse acc))))
+    (rec list more-lists nil)))
+
+;;;
+;;; Body
+;;;
+
+;; 0の数が異なっていればどう操作しても
+;; sとuは一致しない。
+;; 0の数が同じならば一致させられる。
+;; 先頭から順に0の位置をマッチングしていく。
+;; 位置が異なっているマッチングの数が答え。
+(defun solve (n s u)
+  (declare (ignore n))
+  (let ((s (map 'list #'digit-char-p s))
+        (u (map 'list #'digit-char-p u)))
+    (when (= (count 0 s) (count 0 u))
+      (length (filter-map #'/=
+                          (position-all 0 s)
+                          (position-all 0 u))))))
+
+(defun main ()
+  (readlet (n)
+    (let ((s (read-line))
+          (u (read-line)))
+      (println (or (solve n s u)
+                   -1)))))
+
+#-swank (main)
