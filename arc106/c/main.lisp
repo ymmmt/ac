93f9b6f61363707ccb3d7d087fa5ca90dfb1508b
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-08-18 22:13:39 +0900

    add file

---
+++ arc106/c/main.lisp
@@ -0,0 +1,200 @@
+(in-package :cl-user)
+
+(unless (member :child-sbcl *features*)
+  (quit
+   :recklessly-p t
+   :unix-status
+   (process-exit-code
+    (run-program *runtime-pathname*
+                 `("--control-stack-size" "256MB"
+                   "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
+                   "--eval" "(push :child-sbcl *features*)"
+                   "--script" ,(namestring *load-pathname*))
+                 :output t :error t :input t))))
+
+(macrolet ((def (b)
+             `(progn (deftype ,(intern (format nil "UINT~A" b)) () '(unsigned-byte ,b))
+                     (deftype ,(intern (format nil "INT~A" b)) () '(signed-byte ,b))))
+           (define-int-types (&rest bits) `(progn ,@(mapcar (lambda (b) `(def ,b)) bits))))
+  (define-int-types 2 4 7 8 15 16 31 32 62 63 64))
+
+(defconstant +mod+ 1000000007)
+
+(defmacro dbg (&rest forms)
+  (declare (ignorable forms))
+  (if (= (length forms) 1)
+      `(format t "~A => ~A~%" ',(car forms) ,(car forms))
+      `(format t "~A => ~A~%" ',forms `(,,@forms))))
+
+(defmethod print-object ((object hash-table) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream ":count ~A :test ~A"
+            (hash-table-count object)
+            (hash-table-test object))
+    (loop for k being the hash-key of object
+          do (format stream "~%[~A] ~A" k (gethash k object)))))
+
+(declaim (inline println printlns))
+(defun println (obj &optional (stream *standard-output*))
+  (let ((*read-default-float-format*
+          (if (typep obj 'double-float) 'double-float *read-default-float-format*)))
+    (prog1 (princ obj stream) (terpri stream))))
+
+(defun printlns (lst)
+  (format t "~{~A~%~}" lst))
+
+;; BEGIN_INSERTED_CONTENTS
+(defpackage :cp/read-fixnum
+  (:use :cl)
+  (:export #:read-fixnum))
+(in-package :cp/read-fixnum)
+
+(declaim (ftype (function * (values fixnum &optional)) read-fixnum))
+(defun read-fixnum (&optional (in *standard-input*))
+  "NOTE: cannot read -2^62"
+  (macrolet ((%read-byte ()
+               `(the (unsigned-byte 8)
+                     #+swank (char-code (read-char in nil #\Nul))
+                     #-swank (sb-impl::ansi-stream-read-byte in nil #.(char-code #\Nul) nil))))
+    (let* ((minus nil)
+           (result (loop (let ((byte (%read-byte)))
+                           (cond ((<= 48 byte 57)
+                                  (return (- byte 48)))
+                                 ((zerop byte) ; #\Nul
+                                  (error "Read EOF or #\Nul."))
+                                 ((= byte #.(char-code #\-))
+                                  (setq minus t)))))))
+      (declare ((integer 0 #.most-positive-fixnum) result))
+      (loop
+        (let* ((byte (%read-byte)))
+          (if (<= 48 byte 57)
+              (setq result (+ (- byte 48)
+                              (* 10 (the (integer 0 #.(floor most-positive-fixnum 10))
+                                         result))))
+              (return (if minus (- result) result))))))))
+
+;; BEGIN_USE_PACKAGE
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package :cp/read-fixnum :cl-user))
+
+;;
+;; Utility
+;;
+
+(in-package :cl-user)
+
+(defmacro readlet (vars &body body)
+  `(let ,(mapcar (lambda (v)
+                   `(,v (read-fixnum)))
+          vars)
+     ,@body))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun ensure-list (x)
+    (if (listp x)
+        x
+        (list x)))
+  (defun string-gensym (symbol)
+    (gensym (string symbol))))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/definitions.lisp#L129
+(defmacro defsubst (name params &body body)
+  `(progn
+     (declaim (inline ,name))
+     (defun ,name ,params
+       ,@body)))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/iter.lisp#L22
+(defmacro nlet (name bindings &body body)
+  (let* ((bindings (mapcar #'ensure-list bindings))
+         (vars  (mapcar #'first bindings))
+         (inits (mapcar #'second bindings))
+         (temps (mapcar #'string-gensym vars))
+         (gtag (string-gensym 'tag)))
+    `(block ,name
+       (let ,(mapcar #'list temps inits)
+         (macrolet ((,name ,vars
+                      `(progn
+                         (psetq
+                          ,@(mapcan #'list
+                                    ',temps
+                                    (list ,@vars)))
+                         (go ,',gtag))))
+           (tagbody
+              ,gtag (return-from ,name
+                      (let ,(mapcar #'list vars temps)
+                        ,@body))))))))
+
+(defmacro dlambda (lambda-list &body body)
+  (let ((gargs (gensym "ARGS")))
+    `(lambda (&rest ,gargs)
+       (destructuring-bind ,lambda-list ,gargs
+         ,@body))))
+
+(defun range (&rest args)
+  (ecase (length args)
+    (1 (let ((end (car args)))
+         (loop for i below end
+               collect i)))
+    (2 (destructuring-bind (start end) args
+         (loop for i from start below end
+               collect i)))
+    (3 (destructuring-bind (start end step) args
+         (loop for i from start below end by step
+               collect i)))))
+
+(defmacro bulk-stdout (&body body)
+  `(write-string
+    (with-output-to-string (*standard-output* nil :element-type 'base-char)
+      ,@body)))
+
+(defun join-print (sequence &key (count (length sequence)) (key #'identity) (test (constantly t)))
+  (let ((c 0))
+    (map nil (lambda (item)
+               (when (and (<= (incf c) count)
+                          (funcall test item))
+                 (princ (funcall key item))
+                 (when (< c count)
+                   (princ " "))))
+         sequence)
+    (fresh-line)))
+
+;;;
+;;; Body
+;;;
+
+(defun unit-interval (left)
+  (list left (1+ left)))
+
+;; 解説: https://atcoder.jp/contests/arc106/editorial/242
+;; と考え方は全く同じ
+(defun solve (n m)
+  (let ((2n (* 2 n))
+        (2m (* 2 m)))
+    (cond ((zerop m)
+           (mapcar #'unit-interval
+                   (range 1 2n 2)))
+          ((plusp m)
+           (when (<= (+ m 2) n)
+             (nconc (mapcar #'unit-interval
+                            (range 2 (+ 2m 3) 2))
+                    (list (list 1 (+ 2m 4)))
+                    (mapcar #'unit-interval
+                            (range (+ 2m 5) (+ 2n 1) 2)))))
+          (t                            ; (minusp m)
+           nil))))
+         
+(defun main ()
+  (readlet (n m)
+    (bulk-stdout
+      (let ((lrs (solve n m)))
+        (if (null lrs)
+            (println -1)
+            (dolist (lr lrs)
+              (join-print lr)))))))
+
+#-swank (main)
