3c99949ff718550cdaf3429b509ef844582aad40
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-08-21 20:37:51 +0900

    add file

---
+++ arc135/b/main.lisp
@@ -0,0 +1,326 @@
+(in-package :cl-user)
+
+(unless (member :child-sbcl *features*)
+  (quit
+   :recklessly-p t
+   :unix-status
+   (process-exit-code
+    (run-program *runtime-pathname*
+                 `("--control-stack-size" "256MB"
+                   "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
+                   "--eval" "(push :child-sbcl *features*)"
+                   "--script" ,(namestring *load-pathname*))
+                 :output t :error t :input t))))
+
+(macrolet ((def (b)
+             `(progn (deftype ,(intern (format nil "UINT~A" b)) () '(unsigned-byte ,b))
+                     (deftype ,(intern (format nil "INT~A" b)) () '(signed-byte ,b))))
+           (define-int-types (&rest bits) `(progn ,@(mapcar (lambda (b) `(def ,b)) bits))))
+  (define-int-types 2 4 7 8 15 16 31 32 62 63 64))
+
+(defconstant +mod+ 1000000007)
+
+(defmacro dbg (&rest forms)
+  (declare (ignorable forms))
+  (if (= (length forms) 1)
+      `(format t "~A => ~A~%" ',(car forms) ,(car forms))
+      `(format t "~A => ~A~%" ',forms `(,,@forms))))
+
+(defmethod print-object ((object hash-table) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream ":count ~A :test ~A"
+            (hash-table-count object)
+            (hash-table-test object))
+    (loop for k being the hash-key of object
+          do (format stream "~%[~A] ~A" k (gethash k object)))))
+
+(declaim (inline println printlns))
+(defun println (obj &optional (stream *standard-output*))
+  (let ((*read-default-float-format*
+          (if (typep obj 'double-float) 'double-float *read-default-float-format*)))
+    (prog1 (princ obj stream) (terpri stream))))
+
+(defun printlns (lst)
+  (format t "~{~A~%~}" lst))
+
+;; BEGIN_INSERTED_CONTENTS
+(defpackage :cp/read-fixnum
+  (:use :cl)
+  (:export #:read-fixnum))
+(in-package :cp/read-fixnum)
+
+(declaim (ftype (function * (values fixnum &optional)) read-fixnum))
+(defun read-fixnum (&optional (in *standard-input*))
+  "NOTE: cannot read -2^62"
+  (macrolet ((%read-byte ()
+               `(the (unsigned-byte 8)
+                     #+swank (char-code (read-char in nil #\Nul))
+                     #-swank (sb-impl::ansi-stream-read-byte in nil #.(char-code #\Nul) nil))))
+    (let* ((minus nil)
+           (result (loop (let ((byte (%read-byte)))
+                           (cond ((<= 48 byte 57)
+                                  (return (- byte 48)))
+                                 ((zerop byte) ; #\Nul
+                                  (error "Read EOF or #\Nul."))
+                                 ((= byte #.(char-code #\-))
+                                  (setq minus t)))))))
+      (declare ((integer 0 #.most-positive-fixnum) result))
+      (loop
+        (let* ((byte (%read-byte)))
+          (if (<= 48 byte 57)
+              (setq result (+ (- byte 48)
+                              (* 10 (the (integer 0 #.(floor most-positive-fixnum 10))
+                                         result))))
+              (return (if minus (- result) result))))))))
+
+;; BEGIN_USE_PACKAGE
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package :cp/read-fixnum :cl-user))
+
+;;
+;; Utility
+;;
+
+(in-package :cl-user)
+
+(defmacro readlet (vars &body body)
+  `(let ,(mapcar (lambda (v)
+                   `(,v (read-fixnum)))
+          vars)
+     ,@body))
+
+(defmacro with-gensyms (syms &body body)
+  `(let ,(mapcar #'(lambda (s)
+                     `(,s (gensym)))
+          syms)
+     ,@body))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/definitions.lisp#L129
+(defmacro defsubst (name params &body body)
+  `(progn
+     (declaim (inline ,name))
+     (defun ,name ,params
+       ,@body)))
+
+(defmacro defmemo ((name &key (key ''first) (test ''eql)) lambda-list &body body)
+  (with-gensyms (table args k val found-p)
+    `(let ((,table (make-hash-table :test ,test)))
+       (defun ,name (&rest ,args)
+         (let ((,k (funcall ,key ,args)))
+           (multiple-value-bind (,val ,found-p)
+               (gethash ,k ,table)
+             (if ,found-p ,val
+                 (setf (gethash ,k ,table)
+                       (destructuring-bind ,lambda-list ,args
+                         ,@body)))))))))
+
+(defmacro with-memo ((name lambda-list &body definition) &body body)
+  (with-gensyms (table args val found-p)
+    `(let ((,table (make-hash-table :test 'equal)))
+       (labels ((,name (&rest ,args)
+                  (multiple-value-bind (,val ,found-p)
+                      (gethash ,args ,table)
+                    (if ,found-p ,val
+                        (setf (gethash ,args ,table)
+                              (destructuring-bind ,lambda-list ,args
+                                ,@definition))))))
+         ,@body))))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun ensure-list (x)
+    (if (listp x)
+        x
+        (list x)))
+  (defun string-gensym (symbol)
+    (gensym (string symbol))))
+
+;; from serapeum
+;; https://github.com/ruricolist/serapeum/blob/master/LICENSE.txt
+;; https://github.com/ruricolist/serapeum/blob/master/iter.lisp#L22
+(defmacro nlet (name bindings &body body)
+  (let* ((bindings (mapcar #'ensure-list bindings))
+         (vars  (mapcar #'first bindings))
+         (inits (mapcar #'second bindings))
+         (temps (mapcar #'string-gensym vars))
+         (gtag (string-gensym 'tag)))
+    `(block ,name
+       (let ,(mapcar #'list temps inits)
+         (macrolet ((,name ,vars
+                      `(progn
+                         (psetq
+                          ,@(mapcan #'list
+                                    ',temps
+                                    (list ,@vars)))
+                         (go ,',gtag))))
+           (tagbody
+              ,gtag (return-from ,name
+                      (let ,(mapcar #'list vars temps)
+                        ,@body))))))))
+
+(defmacro dlambda (lambda-list &body body)
+  (let ((gargs (gensym "ARGS")))
+    `(lambda (&rest ,gargs)
+       (destructuring-bind ,lambda-list ,gargs
+         ,@body))))
+
+(defun readvector (&rest args)
+  (values (read-from-string
+           (concatenate 'string "#("
+                        (apply #'read-line args)
+                        ")"))))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun range (&rest args)
+    (ecase (length args)
+      (1 (let ((end (car args)))
+           (loop for i below end
+                 collect i)))
+      (2 (destructuring-bind (start end) args
+           (loop for i from start below end
+                 collect i)))
+      (3 (destructuring-bind (start end step) args
+           (loop for i from start below end by step
+                 collect i))))))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun var-and-dimension-spec->loop (var-and-dimension-spec body)
+    (destructuring-bind (var upper-bound &key downward) var-and-dimension-spec
+      (if downward
+          `(loop for ,var from (1- ,upper-bound) downto 0
+                 do ,body)
+          `(loop for ,var below ,upper-bound
+                 do ,body)))))
+
+(defmacro make-array-with-content (var-and-dimension-specs &body body)
+  `(let ((self (make-array (list ,@(mapcar #'second var-and-dimension-specs)))))
+     ,(reduce (lambda (var-and-dimension-spec acc)
+                (var-and-dimension-spec->loop var-and-dimension-spec acc))
+              var-and-dimension-specs
+              :from-end t
+              :initial-value `(setf (aref self ,@(mapcar #'first var-and-dimension-specs))
+                                    (progn ,@body)))
+     self))
+
+(defun pad (vector)
+  (concatenate 'vector #(0) vector))
+
+(defun scanl (function initial-value list)
+  (labels ((rec (list acc)
+             (if (null list)
+                 (nreverse acc)
+                 (rec (cdr list) (cons (funcall function (car acc) (car list))
+                                       acc)))))
+    (rec list (list initial-value))))
+
+(defun scanl1 (function list)
+  (if (null list)
+      (error "scanl1: empty list")
+      (scanl function (car list) (cdr list))))
+
+(defun position-filter (predicate sequence)
+  (let ((pos -1))
+    (remove-if-not (lambda (item)
+                     (declare (ignore item))
+                     (funcall predicate (incf pos)))
+                   sequence)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun mksym (control-string &rest format-arguments)
+    (intern (apply #'format nil control-string format-arguments))))
+
+(defmacro aif (test then &optional else)
+  `(let ((it ,test))
+     (if it ,then ,else)))
+
+(defun join-print (sequence &key (count (length sequence)) (key #'identity) (test (constantly t)))
+  (let ((c 0))
+    (map nil (lambda (item)
+               (when (and (<= (incf c) count)
+                          (funcall test item))
+                 (princ (funcall key item))
+                 (when (< c count)
+                   (princ " "))))
+         sequence)
+    (fresh-line)))
+
+(defun conjoin (predicate &rest more-predicates)
+  (if (null more-predicates)
+      predicate
+      (lambda (&rest arguments)
+        (and (apply predicate arguments)
+             (do ((tail (cdr more-predicates) (cdr tail))
+                  (head (car more-predicates) (car tail)))
+                 ((not tail)
+                  (apply head arguments))
+               (unless (apply head arguments)
+                 (return nil)))))))
+
+;;;
+;;; Body
+;;;
+
+;; depends on range at compile time
+(defmacro define-mod-predicates (divisor)
+  (assert (integerp divisor))
+  `(progn
+     ,@(mapcar (lambda (r)
+                 `(defun ,(mksym "~DMOD~DP" r divisor) (x)
+                    (= (mod x ,divisor) ,r)))
+               (range divisor))))
+
+(define-mod-predicates 3)
+
+(defun instance (a1 a2 a3 s n)
+  (let ((as (list 0 a1 a2 a3)))
+    (subseq (make-array-with-content ((i (+ n 3)))
+              (if (<= i 3)
+                  (nth i as)
+                  (+ (svref self (- i 3))
+                     (- (svref s (- i 3)))
+                     (svref s (- i 2)))))
+            1)))
+  
+;; a1とa2が定まれば、a3からa[n+2]はすべて定まる。
+;; 一般式は、i = k (mod 3)として
+;; ai = ak - sk + s[k+1] - s[k+3] + s[k+4] ... s[i-3] + s[i-2]
+;; となる。たとえばi=8なら
+;; a8 = a2 - s2 + s3 - s5 + s6
+;; など。
+;; 任意のiに対してai>=0という条件があるから、
+;; この式の左辺から、ak(0<=k<3)の下限が定まる。
+;; 再びi=8なら、
+;; a2 >= s2 - s3 + s5 - s6
+;; という下限が出てくる。これらの下限のmaxをとることで、
+;; a1、a2、a3の下限が求まる。その下限のもとで
+;; a1 + a2 + a3 = s1を満たす数が存在すれば答えはYes。
+(defun solve (n s)
+  (let ((s (pad s)))
+    (let ((sums (mapcar (lambda (i)
+                          (- (svref s i)
+                             (svref s (1+ i))))
+                        (range n))))
+      ;; lb: lower bound
+      (labels ((lb (pred)
+                 (max 0
+                      (reduce #'max
+                              (scanl #'+ 0 (position-filter pred sums))))))
+        (let ((a1-lb (lb #'1mod3p))
+              (a2-lb (lb #'2mod3p))
+              (a3-lb (lb (conjoin #'0mod3p #'plusp)))
+              (s1 (svref s 1)))
+          (when (<= (+ a1-lb a2-lb a3-lb)
+                    s1)
+            (instance a1-lb a2-lb (- s1 a1-lb a2-lb) s n)))))))
+
+(defun main ()
+  (readlet (n)
+    (let ((s (readvector)))
+      (aif (solve n s)
+           (progn
+             (println "Yes")
+             (join-print it))
+           (println "No")))))
+                    
+#-swank (main)
