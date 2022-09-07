7149a2bd7a8c1ce360549fafe9b89675d2ab29dd
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-09-07 23:47:09 +0900

    add cp/mod-operations to template

--- main.lisp
+++ main.lisp
@@ -641,6 +641,62 @@ INITIAL-ARGS == (initial-arg1 initial-arg2 ... initial-argN)"
 (defsubst md (x)
   (mod x +mod+))
 
+;; from cp/mod-operations
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *modulus* 0))
+(declaim ((unsigned-byte 31) *modulus*)
+         #+sbcl (sb-ext:always-bound *modulus*))
+
+(defmacro define-mod-operations
+    (divisor &optional (package #+sbcl (sb-int:sane-package) #-sbcl *package*))
+  (let ((mod* (intern "MOD*" package))
+        (mod+ (intern "MOD+" package))
+        (mod- (intern "MOD-" package))
+        (incfmod (intern "INCFMOD" package))
+        (decfmod (intern "DECFMOD" package))
+        (mulfmod (intern "MULFMOD" package)))
+    `(progn
+       (defun ,mod* (&rest args)
+         (cond ((cdr args) (reduce (lambda (x y) (mod (* x y) ,divisor)) args))
+               (args (mod (car args) ,divisor))
+               (t (mod 1 ,divisor))))
+       (defun ,mod+ (&rest args)
+         (cond ((cdr args) (reduce (lambda (x y) (mod (+ x y) ,divisor)) args))
+               (args (mod (car args) ,divisor))
+               (t 0)))
+       (defun ,mod- (&rest args)
+         (if (cdr args)
+             (reduce (lambda (x y) (mod (- x y) ,divisor)) args)
+             (mod (- (car args)) ,divisor)))
+
+       #+sbcl
+       (eval-when (:compile-toplevel :load-toplevel :execute)
+         (locally (declare (sb-ext:muffle-conditions warning))
+           (sb-c:define-source-transform ,mod* (&rest args)
+             (case (length args)
+               (0 `(mod 1 ,',divisor))
+               (1 `(mod ,(car args) ,',divisor))
+               (otherwise (reduce (lambda (x y) `(mod (* ,x ,y) ,',divisor)) args))))
+           (sb-c:define-source-transform ,mod+ (&rest args)
+             (case (length args)
+               (0 0)
+               (1 `(mod ,(car args) ,',divisor))
+               (otherwise (reduce (lambda (x y) `(mod (+ ,x ,y) ,',divisor)) args))))
+           (sb-c:define-source-transform ,mod- (&rest args)
+             (case (length args)
+               (0 (values nil t))
+               (1 `(mod (- ,(car args)) ,',divisor))
+               (otherwise (reduce (lambda (x y) `(mod (- ,x ,y) ,',divisor)) args))))))
+
+       (define-modify-macro ,incfmod (delta)
+         (lambda (x y) (mod (+ x y) ,divisor)))
+       (define-modify-macro ,decfmod (delta)
+         (lambda (x y) (mod (- x y) ,divisor)))
+       (define-modify-macro ,mulfmod (multiplier)
+         (lambda (x y) (mod (* x y) ,divisor))))))
+
+(define-mod-operations +mod+)
+
 (defsubst oddity= (x y)
   (= (mod x 2) (mod y 2)))
 
