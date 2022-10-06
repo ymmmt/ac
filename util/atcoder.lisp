530b6d9a368dcc92600181244a5a3de37a97b9af
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-10-07 04:38:05 +0900

    add utility macro and function: define, arglist

--- util/atcoder.lisp
+++ util/atcoder.lisp
@@ -1,3 +1,36 @@
+(defmacro define (name closure)
+  (assert (symbolp name))
+  (sb-ext::once-only ((closure closure))
+    `(progn
+       (defun ,name (&rest args)
+         (apply ,closure args))
+       (setf (fdefinition ',name)
+             ,closure))))
+
+;; https://stackoverflow.com/questions/15465138/find-functions-arity-in-common-lisp
+;; https://sourceforge.net/projects/clocc/
+(defun arglist (fn)
+  "Return the signature of the function."
+  #+allegro (excl:arglist fn)
+  #+clisp (sys::arglist fn)
+  #+(or cmu scl)
+  (let ((f (coerce fn 'function)))
+    (typecase f
+      (STANDARD-GENERIC-FUNCTION (pcl:generic-function-lambda-list f))
+      (EVAL:INTERPRETED-FUNCTION (eval:interpreted-function-arglist f))
+      (FUNCTION (values (read-from-string (kernel:%function-arglist f))))))
+  #+cormanlisp (ccl:function-lambda-list
+                (typecase fn (symbol (fdefinition fn)) (t fn)))
+  #+gcl (let ((fn (etypecase fn
+                    (symbol fn)
+                    (function (si:compiled-function-name fn)))))
+          (get fn 'si:debug))
+  #+lispworks (lw:function-lambda-list fn)
+  #+lucid (lcl:arglist fn)
+  #+sbcl (sb-introspect:function-lambda-list fn)
+  #-(or allegro clisp cmu cormanlisp gcl lispworks lucid sbcl scl)
+  (error 'not-implemented :proc (list 'arglist fn)))
+
 (defmacro portably-without-package-locks (&body body)
   `(#+sbcl sb-ext:without-package-locks
     #+allegro excl::without-package-locks
