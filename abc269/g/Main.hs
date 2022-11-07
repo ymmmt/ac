4341ddb3bc6a28209c8d050bda946c3b5137c5ee
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-11-08 00:35:37 +0900

    tmp: compile success

--- abc269/g/Main.hs
+++ abc269/g/Main.hs
@@ -1,19 +1,23 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 import Control.Monad
-import qualified Data.ByteString.Char8 as BS
-import qualified Data.Map as Map
-import Data.Maybe
+import Control.Monad.ST
+import Data.Array.MArray
+import Data.Array.ST
+import Data.Array.Unboxed
+import Data.Char
 import Data.Function
 import Data.List
+import Data.Maybe
 import Data.Tuple
-import Data.Char
-import Data.Vector.Unboxed (Vector, (!), (//), fromList)
+--import Data.Vector.Unboxed (Vector, (!), (//), fromList)
 import Debug.Trace
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Map as Map
 
 readInt :: IO Int
 readInt = fst . fromJust . BS.readInt <$> BS.getLine
@@ -43,12 +47,23 @@ convert xs = concatMap (pairs c) ks
   where c = counter xs
         ks = map fst $ Map.toList c
 
-update :: Int -> (Int, Int) -> Vector Int -> Vector Int
-update m (c, count) dp = dp // map new [l..r]
-  where d = c * count
-        l = max 0 (- d)
-        r = min m (m - d)
-        new s = (s+d, min (dp!(s+d)) (dp!s + count))
+-- update :: Int -> (Int, Int) -> STUArray ? Int Int -> STUArray ? Int Int
+-- update m (c, count) dp = dp // map new [l..r]
+--   where d = c * count
+--         l = max 0 (- d)
+--         r = min m (m - d)
+--         new s = (s+d, min (dp!(s+d)) (dp!s + count))
+
+update :: Int -> (STUArray s Int Int) -> (Int, Int) -> ST s (STUArray s Int Int)
+update m dp (c, count) =
+  let d   = c * count
+      l   = max 0 (- d)
+      r   = min m (m - d)
+      ss  = if d>=0 then [r, r-1..l] else [l..r]
+  in do forM_ ss $ \s -> do x <- readArray dp (s+d)
+                            y <- readArray dp s
+                            writeArray dp s $ min x (y+count)
+        return dp
 
 solve :: Int -> Int -> [[Int]] -> [Int]
 solve n m abs = map (ans n dp) [0..m]
@@ -56,8 +71,12 @@ solve n m abs = map (ans n dp) [0..m]
         fs = convert cs
         sa = sum $ map head abs
         inf = n+1
-        dp0 = (fromList $ take (m+1) (repeat inf)) // [(sa, 0)]
-        dp = foldr (update m) dp0 fs
+        --dp0 = (fromList $ take (m+1) (repeat inf)) // [(sa, 0)]
+        dp = runSTUArray $ do
+          dp' <- newArray (0, m) inf :: ST s (STUArray s Int Int)
+          writeArray dp' sa 0
+          forM_ fs (update m dp')
+          return dp'
         ans n dp s = if dp!s <= n then dp!s else -1
 
 main :: IO ()
