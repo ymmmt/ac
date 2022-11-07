3c003c7dbf1ca174a6d8da6374865979386b7967
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-11-08 03:54:16 +0900

    add hs template

---
+++ Main.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Control.Monad
+import Data.Bits
+import Data.Char
+import Data.Function
+import Data.Int
+import Data.Ix
+import Data.List
+import Data.Maybe
+import Data.Tuple
+import Debug.Trace
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Map as Map
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as UMV
+
+readInt :: IO Int
+readInt = fst . fromJust . BS.readInt <$> BS.getLine
+
+readIntList :: IO [Int]
+readIntList = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+
+readIntLists :: Int -> IO [[Int]]
+readIntLists n = replicateM n readIntList
+
+main :: IO ()
+main = do
