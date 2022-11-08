0f23cd7056ae0a96d4fcbc4d99e0453d522b5104
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-11-08 16:48:10 +0900

    add utility function: joinStr

--- Util.hs
+++ Util.hs
@@ -1,13 +1,16 @@
+-- IO
+
 yn :: Bool -> String
 yn True  = "Yes"
 yn False = "No"
 
+joinStr :: Show a => String -> [a] -> String
+joinStr sep xs = intercalate sep $ map show xs
+
+-- List
+
 sortUniq :: Ord a => [a] -> [a]
 sortUniq = map head . group . sort
-
-counter :: Ord a => [a] -> Map.Map a Int
-counter = foldr (\x mp -> Map.insertWith f x 1 mp) Map.empty
-  where f _ old = old + 1
 
 pairs1 :: [a] -> [(a, a)]
 pairs1 []     = []
@@ -17,6 +20,14 @@ pairs1 (x:xs) = [(x, y) | y <- xs]
 pairs :: [a] -> [(a, a)]
 pairs = concatMap pairs1 . tails
 
+-- Map
+
+counter :: Ord a => [a] -> Map.Map a Int
+counter = foldr (\x mp -> Map.insertWith f x 1 mp) Map.empty
+  where f _ old = old + 1
+
+-- Function
+
 pair :: (a -> b, a -> c) -> a -> (b, c)
 pair (f, g) x = (f x, g x)
 
@@ -25,6 +36,8 @@ cross (f, g) = pair (f . fst, g . snd)
 
 fork :: (a -> b) -> (a, a) -> (b, b)
 fork f (x, y) = (f x, f y)
+
+-- Math
 
 arithSeqSum :: Integral a => a -> a -> a -> a
 arithSeqSum n d a0 = n * a0 + d * n * (n-1) `div` 2
