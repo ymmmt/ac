8568b47a0aad763ed2bfc265f931bbe13bfccb41
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-11-14 11:14:54 +0900

    add utility functions: qt, modInt

--- Util.hs
+++ Util.hs
@@ -73,6 +73,10 @@ fork f (x, y) = (f x, f y)
 
 -- Math
 
+qt :: Bool -> Int
+qt True  = 1
+qt False = 0
+
 arithSeqSum :: Integral a => a -> a -> a -> a
 arithSeqSum n d a0 = n * a0 + d * n * (n-1) `div` 2
 
@@ -85,6 +89,31 @@ mod' k n
   | m < 0 = m + n
   | otherwise = m
   where m = mod k n
+
+modulo :: Int
+modulo = 998244353
+
+-- https://rosettacode.org/wiki/Modular_inverse
+modInv :: Int -> Int -> Maybe Int
+modInv a m
+  | 1 == g = Just (mkPos i)
+  | otherwise = Nothing
+  where
+    (i, _, g) = gcdExt a m
+    mkPos x
+      | x < 0 = x + m
+      | otherwise = x
+
+-- Extended Euclidean algorithm.
+-- Given non-negative a and b, return x, y and g
+-- such that ax + by = g, where g = gcd(a,b).
+-- Note that x or y may be negative.
+gcdExt :: Int -> Int -> (Int, Int, Int)
+gcdExt a 0 = (1, 0, a)
+gcdExt a b =
+  let (q, r) = a `quotRem` b
+      (s, t, g) = gcdExt b r
+  in (t, s - q * t, g)
 
 class Group a where
   gzero    :: a
