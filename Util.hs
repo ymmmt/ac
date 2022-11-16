b921c09b990ce1acaa87c9e08b2271615f0a75bf
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-11-16 20:30:52 +0900

    add utility function: average

--- Util.hs
+++ Util.hs
@@ -81,6 +81,9 @@ fork f (x, y) = (f x, f y)
 arithSeqSum :: Integral a => a -> a -> a -> a
 arithSeqSum n d a0 = n * a0 + d * n * (n-1) `div` 2
 
+average :: [Double] -> Double
+average xs = sum xs / fromIntegral (length xs)
+
 cut :: Ord a => a -> a -> a -> a
 cut l h | l > h     = undefined
         | otherwise = max l . min h
