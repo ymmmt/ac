fc32f529f6889c5465feebb0852b0c13c60a38d2
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-11-17 16:09:07 +0900

    add file

---
+++ abc277/c/Main.hs
@@ -0,0 +1,256 @@
+{-# OPTIONS_GHC -O2 #-}
+
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Control.Applicative
+import Control.Monad
+import Data.Array
+import Data.Bits
+import Data.Char
+import Data.Foldable
+import Data.Function
+import Data.Int
+import Data.Ix
+import Data.List
+import Data.Maybe
+import Data.Tuple
+import Data.Void
+import Data.Word
+import Debug.Trace
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Graph as G
+import qualified Data.Map as Map
+import qualified Data.Ratio as R
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as UMV
+import qualified Numeric as N
+
+readInt :: IO Int
+readInt = fst . fromJust . BS.readInt <$> BS.getLine
+
+readTuples :: Int -> IO [(Int, Int)]
+readTuples n = replicateM n (t <$> readIntList)
+  where t [x, y] = (x, y)
+
+readIntList :: IO [Int]
+readIntList = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+
+readIntLists :: Int -> IO [[Int]]
+readIntLists n = replicateM n readIntList
+
+-- Data.Function.Memoize
+
+class Memoizable a where
+  memoize :: (a -> v) -> a -> v
+
+memoize2 :: (Memoizable a, Memoizable b) =>
+            (a -> b -> v) -> a -> b -> v
+memoize2 v = memoize (memoize . v)
+
+memoize3 :: (Memoizable a, Memoizable b, Memoizable c) =>
+            (a -> b -> c -> v) -> a -> b -> c -> v
+memoize3 v = memoize (memoize2 . v)
+
+memoize4 :: (Memoizable a, Memoizable b, Memoizable c, Memoizable d) =>
+            (a -> b -> c -> d -> v) -> a -> b -> c -> d -> v
+memoize4 v = memoize (memoize3 . v)
+
+memoize5 :: (Memoizable a, Memoizable b, Memoizable c, Memoizable d,
+             Memoizable e) =>
+            (a -> b -> c -> d -> e -> v) -> a -> b -> c -> d -> e -> v
+memoize5 v = memoize (memoize4 . v)
+
+memoize6 :: (Memoizable a, Memoizable b, Memoizable c, Memoizable d,
+             Memoizable e, Memoizable f) =>
+            (a -> b -> c -> d -> e -> f -> v) ->
+            a -> b -> c -> d -> e -> f -> v
+memoize6 v = memoize (memoize5 . v)
+
+memoize7 :: (Memoizable a, Memoizable b, Memoizable c, Memoizable d,
+             Memoizable e, Memoizable f, Memoizable g) =>
+            (a -> b -> c -> d -> e -> f -> g -> v) ->
+            a -> b -> c -> d -> e -> f -> g -> v
+memoize7 v = memoize (memoize6 . v)
+
+memoFix :: Memoizable a => ((a -> v) -> a -> v) -> a -> v
+memoFix ff = f where f = memoize (ff f)
+
+memoFix2 :: (Memoizable a, Memoizable b) =>
+            ((a -> b -> v) -> a -> b -> v) -> a -> b -> v
+memoFix2 ff = f where f = memoize2 (ff f)
+
+memoFix3 :: (Memoizable a, Memoizable b, Memoizable c) =>
+            ((a -> b -> c -> v) -> a -> b -> c -> v) -> a -> b -> c -> v
+memoFix3 ff = f where f = memoize3 (ff f)
+
+memoFix4 :: (Memoizable a, Memoizable b, Memoizable c, Memoizable d) =>
+            ((a -> b -> c -> d -> v) -> (a -> b -> c -> d -> v)) ->
+            a -> b -> c -> d -> v
+memoFix4 ff = f where f = memoize4 (ff f)
+
+memoFix5 :: (Memoizable a, Memoizable b, Memoizable c, Memoizable d,
+             Memoizable e) =>
+            ((a -> b -> c -> d -> e -> v) -> (a -> b -> c -> d -> e -> v)) ->
+            a -> b -> c -> d -> e -> v
+memoFix5 ff = f where f = memoize5 (ff f)
+
+memoFix6 :: (Memoizable a, Memoizable b, Memoizable c, Memoizable d,
+             Memoizable e, Memoizable f) =>
+            ((a -> b -> c -> d -> e -> f -> v) -> (a -> b -> c -> d -> e -> f -> v)) ->
+            a -> b -> c -> d -> e -> f -> v
+memoFix6 ff = f where f = memoize6 (ff f)
+
+memoFix7 :: (Memoizable a, Memoizable b, Memoizable c, Memoizable d,
+             Memoizable e, Memoizable f, Memoizable g) =>
+            ((a -> b -> c -> d -> e -> f -> g -> v) -> (a -> b -> c -> d -> e -> f -> g -> v)) ->
+            a -> b -> c -> d -> e -> f -> g -> v
+memoFix7 ff = f where f = memoize7 (ff f)
+
+traceMemoize :: (Memoizable a, Show a) => (a -> b) -> a -> b
+traceMemoize f = memoize (\a -> traceShow a (f a))
+
+data BinaryTreeCache v = BinaryTreeCache {
+  btValue         :: v,
+  btLeft, btRight :: BinaryTreeCache v
+  }
+  deriving Functor
+
+newtype Finite a = ToFinite { fromFinite :: a }
+  deriving (Eq, Bounded, Enum)
+
+instance (Bounded a, Enum a) => Memoizable (Finite a) where
+  memoize f = finiteLookup (f <$> theFinites)
+
+theFinites :: (Bounded a, Enum a) => BinaryTreeCache a
+theFinites = loop minBound maxBound
+  where loop start stop = BinaryTreeCache {
+          btValue = mean,
+          btLeft  = loop start (pred mean),
+          btRight = loop (succ mean) stop
+          }
+          where mean = meanFinite start stop
+
+finiteLookup :: (Bounded a, Enum a) => BinaryTreeCache v -> a -> v
+finiteLookup cache0 a0 = loop start0 stop0 cache0
+  where start0 = fromEnum (minBound `asTypeOf` a0)
+        stop0  = fromEnum (maxBound `asTypeOf` a0)
+        a      = fromEnum a0
+        loop start stop cache =
+          let mean = meanFinite start stop in
+            case a `compare` mean of
+              EQ -> btValue cache
+              LT -> loop start (pred mean) (btLeft cache)
+              GT -> loop (succ mean) stop (btRight cache)
+
+meanFinite :: (Bounded a, Enum a) => a -> a -> a
+meanFinite a b = toEnum (ia `div` 2 + ib `div` 2 +
+                          if odd ia && odd ib then 1 else 0)
+  where ia = fromEnum a
+        ib = fromEnum b
+
+memoizeFinite :: (Enum a, Bounded a) => (a -> v) -> a -> v
+memoizeFinite f = memoize (f . fromFinite) . ToFinite
+
+instance Memoizable Int    where memoize = memoizeFinite
+instance Memoizable Char   where memoize = memoizeFinite
+instance Memoizable Word   where memoize = memoizeFinite
+instance Memoizable Word8  where memoize = memoizeFinite
+instance Memoizable Word16 where memoize = memoizeFinite
+instance Memoizable Word32 where memoize = memoizeFinite
+instance Memoizable Word64 where memoize = memoizeFinite
+
+-- deriveMemoizable ''()
+-- deriveMemoizable ''Bool
+-- deriveMemoizable ''Ordering
+-- deriveMemoizable ''Maybe
+-- deriveMemoizable ''Either
+-- deriveMemoizable ''[]
+-- deriveMemoizable ''Complex.Complex
+-- deriveMemoizable ''Version.Version
+
+-- deriveMemoizable ''Tuple.Solo
+
+-- deriveMemoizable ''(,)
+-- deriveMemoizable ''(,,)
+-- deriveMemoizable ''(,,,)
+-- deriveMemoizable ''(,,,,)
+-- deriveMemoizable ''(,,,,,)
+-- deriveMemoizable ''(,,,,,,)
+-- deriveMemoizable ''(,,,,,,,)
+-- deriveMemoizable ''(,,,,,,,,)
+-- deriveMemoizable ''(,,,,,,,,,)
+-- deriveMemoizable ''(,,,,,,,,,,)
+-- deriveMemoizable ''(,,,,,,,,,,,)
+
+encodeInteger :: Integer -> [Int]
+encodeInteger 0 = []
+encodeInteger i | minInt <= i && i <= maxInt
+                = [fromInteger i]
+encodeInteger i = fromInteger (i .&. maxInt) : encodeInteger (i `shiftR` intBits)
+
+decodeInteger :: [Int] -> Integer
+decodeInteger = foldr op 0
+  where op i i' = fromIntegral i .|. i' `shiftL` intBits
+
+intBits :: Int
+intBits = finiteBitSize (0 :: Int) - 1
+
+minInt, maxInt :: Integer
+minInt = fromIntegral (minBound :: Int)
+maxInt = fromIntegral (maxBound :: Int)
+
+instance (Eq a, Bounded a, Enum a, Memoizable b) => Memoizable (a -> b) where
+  memoize = functionLookup . theFunctions
+
+functionLookup :: (Eq a, Bounded a, Enum a, Memoizable b) =>
+                  FunctionCache b v -> (a -> b) -> v
+functionLookup cache f =
+  fcNil (foldl fcCons cache (f <$> [minBound .. maxBound]))
+
+theFunctions :: (Eq a, Bounded a, Enum a, Memoizable b) =>
+                ((a -> b) -> v) -> FunctionCache b v
+theFunctions f = FunctionCache {
+  fcNil  = f undefined,
+  fcCons = memoize (\b -> theFunctions (f . extend b))
+  }
+  where extend b g a | a == minBound = b
+                     | otherwise     = g (pred a)
+
+data FunctionCache b v
+  = FunctionCache {
+      fcNil  :: v,
+      fcCons :: b -> FunctionCache b v
+    }
+
+--
+
+sortUniq :: Ord a => [a] -> [a]
+sortUniq = map head . group . sort
+
+coordComp :: Ord a => Int -> [a] -> Map.Map Int a
+coordComp i0 = Map.fromAscList . zip [i0..] . sortUniq
+
+solve :: Int -> [[Int]] -> Int
+solve n abs = if Map.member 1 mp'
+              then mp Map.! (maximum $ G.reachable g 1)
+              else 1
+  where
+    mp           = coordComp 1 $ concat abs
+    mp'          = Map.fromAscList $ zip (Map.elems mp) [1..]
+    edges [a, b] = let a' = mp' Map.! a
+                       b' = mp' Map.! b
+                   in [(a', b'), (b', a')]
+    g            = G.buildG (1, 2*n) $ concatMap edges abs
+
+main :: IO ()
+main = do
+  n <- readInt
+  abs <- readIntLists n
+  print $ solve n abs
