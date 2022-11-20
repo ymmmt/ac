39580d92a68e8cfc8d644d2e51b8dafd0b4e8287
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-11-20 16:53:33 +0900

    try vector shuffle heuristic

---
+++ ahc016/a/Main.hs
@@ -0,0 +1,202 @@
+{-# OPTIONS_GHC -O2 #-}
+
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.State
+import Data.Array
+import Data.Bits
+import Data.Char
+import Data.Function
+import Data.Int
+import Data.Ix
+import Data.List
+import Data.Maybe
+import Data.Tuple
+import Data.Void
+import Data.Word
+import Debug.Trace
+import System.IO
+import System.Random -- random-1.0.1.1
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Graph as G
+import qualified Data.Map as Map
+import qualified Data.Ratio as R
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as UMV
+import qualified Numeric as N
+
+getWords :: IO [String]
+getWords = words <$> getLine
+
+readInt :: IO Int
+readInt = fst . fromJust . BS.readInt <$> BS.getLine
+
+readIntList :: IO [Int]
+readIntList = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+
+readIntLists :: Int -> IO [[Int]]
+readIntLists n = replicateM n readIntList
+
+flush :: IO ()
+flush = hFlush stdout
+
+randomSt :: (RandomGen g, Random a) => State g a
+randomSt = state random
+
+randomRSt :: (RandomGen g, Random a) => (a, a) -> State g a
+randomRSt xy = state (randomR xy)
+
+type Probability = Double
+
+judgeSt :: Probability -> State StdGen Bool
+judgeSt p = (< p) <$> randomRSt (0.0, 1.0)
+
+shuffleStepSt :: Map.Map Int a -> (Int, a) -> State StdGen (Map.Map Int a)
+shuffleStepSt mp (i, x) = do
+  j <- randomRSt (0, i)
+  return . Map.insert j x $ Map.insert i (mp Map.! j) mp
+
+shuffleSt :: [a] -> State StdGen [a]
+shuffleSt [] = return []
+shuffleSt xs = do
+  mp <- foldM shuffleStepSt (Map.singleton 0 (head xs)) $ zip [1..] (tail xs)
+  return $ Map.elems mp
+
+minimumOn :: Ord a => (b -> a) -> [b] -> (b, a, Int)
+minimumOn k xs = foldl1 step $ zip3 xs (map k xs) [0..]
+  where step u@(_, kx, _) v@(_, ky, _) =
+          if kx <= ky then u else v
+
+count :: (a -> Bool) -> [a] -> Int
+count p = length . filter p
+
+d2 :: [Double] -> [Double] -> Double
+d2 xs ys = sum $ zipWith d2' xs ys
+  where d2' x y = (x - y) ** 2
+
+type Graph   = Array (Index, Index) Bool
+type Size    = Int
+type Degree  = Int
+type Edge    = (Index, Index)
+type Index   = Int
+
+type Epsilon = Double
+
+charB :: Bool -> Char
+charB True  = '1'
+charB False = '0'
+
+boolC :: Char -> Bool
+boolC '1' = True
+boolC '0' = False
+boolC _   = undefined
+
+showG :: Size -> Graph -> String
+showG n g = map (charB . (g!)) $ edges n
+
+parseG :: Size -> String -> Graph
+parseG n s = accumArray (||) False ((0, 0), (n-1, n-1))
+             $ zip (edges n) (map boolC s)
+
+edges :: Size -> [Edge]
+edges n = [(i, j) | i <- [0..n-2], j <- [i+1..n-1]]
+
+degrees :: Size -> Graph -> [Degree]
+degrees n g = sort $ map deg [0..n-1]
+  where deg i = count (g!) [(min i j, max i j) | j <- [0..n-1], j /= i]
+
+approximateRegularG :: Size -> Degree -> State StdGen Graph
+approximateRegularG n d = do
+  let p = fromIntegral d / fromIntegral (n-1)
+  bs <- replicateM (n * (n-1) `div` 2) $ judgeSt p
+  let g = accumArray (||) False ((0, 0), (n-1, n-1))
+          $ zip (edges n) bs
+  return g
+
+lindiv :: Size -> Int -> Int
+lindiv n m = n * (n-1) `div` 2 `div` m
+
+aRGs :: Size -> Int -> State StdGen [Graph]
+aRGs n m = mapM (approximateRegularG n) [0, d..d*(m-1)]
+  where d = n `div` m
+
+solveSt :: Int -> Epsilon -> State StdGen (Size, [Graph])
+solveSt m e = (n,) <$> aRGs n m
+  where n = 100
+
+expectedDegree :: Epsilon -> Size -> Degree -> Double
+expectedDegree e n d = (1 - 2 * e) * d' + (n' - 1) * e
+  where n' = fromIntegral n
+        d' = fromIntegral d
+
+dist :: [Double] -> [[Double]] -> Double
+dist ds ess = d
+  where (_, d, _)  = minimumOn (d2 ds) ess
+
+guess :: Size -> Graph -> [[[Double]]] -> Index
+guess n g esss = i
+  where ds        = map fromIntegral $ degrees n g
+        (_, _, i) = minimumOn (dist ds) esss
+
+simulateSt :: Epsilon -> Size -> Graph -> State StdGen Graph
+simulateSt e n g = do
+  let es = edges n
+  bs  <- replicateM (n * (n-1) `div` 2) $ judgeSt e
+  es' <- shuffleSt es
+  let g'  = accum xor g $ zip es bs
+      g'' = accumArray (||) False ((0, 0), (n-1, n-1))
+            . zip es $ map (g'!) es'
+  return g''
+
+-- simulate only edge fliping
+simulateSt' :: Epsilon -> Size -> Graph -> State StdGen Graph
+simulateSt' e n g = do
+  let es = edges n
+  bs  <- replicateM (n * (n-1) `div` 2) $ judgeSt e
+  let g' = accum xor g $ zip es bs
+  return g'
+
+redCount :: Int
+redCount = 75
+
+randomExpectedDegreesSt :: Epsilon -> Size -> [Graph] -> State StdGen [[[Double]]]
+randomExpectedDegreesSt e n gs = mapM degs $ map exps gs
+  where exps = map (expectedDegree e n) . (degrees n)
+        degs = replicateM redCount . shuffleSt
+
+answer :: Size -> [[[Double]]] -> IO ()
+answer n esss = do
+  g <- (parseG n) <$> getLine
+  print $ guess n g esss
+  flush
+
+debugAnswer :: Epsilon -> Size -> [Graph] -> [[[Double]]] -> IO ()
+debugAnswer e n gs esss = do
+  i <- readInt
+  g <- evalState (simulateSt e n (gs!!i)) <$> newStdGen
+  -- print $ "guessing: " ++ show i
+  -- putStrLn $ (showG n) g
+  print $ guess n g esss
+  flush
+
+main :: IO ()
+main = do
+  [sm, se] <- getWords
+  let m = read sm :: Int
+      e = read se :: Epsilon
+  ((n, gs), s) <- runState (solveSt m e) <$> newStdGen
+  putStr . unlines $ [show n] ++ map (showG n) gs
+  flush
+  
+  let esss = evalState (randomExpectedDegreesSt e n gs) s
+  replicateM_ 100 (answer n esss)
+--  replicateM_ 100 (debugAnswer e n gs esss)
