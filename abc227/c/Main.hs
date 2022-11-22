7332fe103a47829af124337fa5e47eabbe3a3c0c
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-11-22 15:45:35 +0900

    add file

---
+++ abc227/c/Main.hs
@@ -0,0 +1,61 @@
+{-# OPTIONS_GHC -O2 #-}
+
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Control.Applicative
+import Control.Monad
+import Data.Array
+import Data.Bits
+import Data.Char
+import Data.Function
+import Data.Int
+import Data.Ix
+import Data.List
+import Data.Maybe
+import Data.Tuple
+import Data.Void
+import Data.Word
+import Debug.Trace
+import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Graph as G
+import qualified Data.Map as Map
+import qualified Data.Ratio as R
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as UMV
+import qualified Numeric as N
+
+readInt :: IO Int
+readInt = fst . fromJust . BS.readInt <$> BS.getLine
+
+readIntList :: IO [Int]
+readIntList = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+
+readIntLists :: Int -> IO [[Int]]
+readIntLists n = replicateM n readIntList
+
+readTuples :: Int -> IO [(Int, Int)]
+readTuples n = replicateM n (t <$> readIntList)
+  where t [x, y] = (x, y)
+
+power :: Int -> Double -> Int
+power x n = floor $ (fromIntegral x) ** n
+
+
+solve :: Int -> Int
+solve n = sum [ n `div` (a * b) - b + 1
+              | a <- takeWhile ((<= n) . (^3)) [1..]
+              , b <- takeWhile ((<= n) . (*a) . (^2)) [a..]]
+
+main :: IO ()
+main = do
+  n <- readInt
+  print $ solve n
