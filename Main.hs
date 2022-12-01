50a2bac66cacee671817c9b84512cf2fdd4104f0
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-12-02 05:47:45 +0900

    minimum declarations of language extensions and modules in template

--- Main.hs
+++ Main.hs
@@ -1,41 +1,41 @@
 {-# OPTIONS_GHC -O2 #-}
 
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeFamilies #-}
+-- {-# LANGUAGE DeriveFunctor #-}
+-- {-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE FlexibleInstances #-}
+-- {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- {-# LANGUAGE MultiParamTypeClasses #-}
+-- {-# LANGUAGE StandaloneDeriving #-}
+-- {-# LANGUAGE TemplateHaskell #-}
+-- {-# LANGUAGE TupleSections #-}
+-- {-# LANGUAGE TypeFamilies #-}
 
-import Control.Applicative ((<*>), Alternative, empty, (<|>))
-import Control.Monad
+-- import Control.Applicative ((<*>), Alternative, empty, (<|>))
+-- import Control.Monad
 -- import Control.Monad.ST
-import Data.Array
+-- import Data.Array
 -- import Data.Array.ST
-import Data.Bits
-import Data.Char
-import Data.Function
-import Data.Int
-import Data.Ix
-import Data.List
-import Data.Maybe
-import Data.Tuple
-import Data.Void
-import Data.Word
-import Debug.Trace
-import qualified Data.Array.Unboxed as UA
-import qualified Data.ByteString.Char8 as BS
-import qualified Data.Graph as G
-import qualified Data.Map as Map
-import qualified Data.Ratio as R
-import qualified Data.Tree as T
-import qualified Data.Set as S
+-- import Data.Bits
+-- import Data.Char
+-- import Data.Function
+-- import Data.Int
+-- import Data.Ix
+-- import Data.List
+-- import Data.Maybe
+-- import Data.Tuple
+-- import Data.Void
+-- import Data.Word
+-- import Debug.Trace
+-- import qualified Data.Array.Unboxed as UA
+-- import qualified Data.ByteString.Char8 as BS
+-- import qualified Data.Graph as G
+-- import qualified Data.Map as Map
+-- import qualified Data.Ratio as R
+-- import qualified Data.Set as S
+-- import qualified Data.Tree as T
 -- import qualified Data.Vector.Unboxed as UV
 -- import qualified Data.Vector.Unboxed.Mutable as UMV
-import qualified Numeric as N
+-- import qualified Numeric as N
 
 tup :: [Int] -> (Int, Int)
 tup [x, y] = (x, y)
