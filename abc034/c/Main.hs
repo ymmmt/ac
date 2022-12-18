9157512e4c6368c2d40ccd7ddd849438bb373766
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-12-18 16:01:51 +0900

    add file

---
+++ abc034/c/Main.hs
@@ -0,0 +1,116 @@
+{-# OPTIONS_GHC -O2 #-}
+
+-- {-# LANGUAGE BangPatterns #-}
+-- {-# LANGUAGE DeriveFunctor #-}
+-- {-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE FlexibleInstances #-}
+-- {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- {-# LANGUAGE MultiParamTypeClasses #-}
+-- {-# LANGUAGE StandaloneDeriving #-}
+-- {-# LANGUAGE TemplateHaskell #-}
+-- {-# LANGUAGE TupleSections #-}
+-- {-# LANGUAGE TypeFamilies #-}
+
+-- import Control.Applicative ((<*>), Alternative, empty, (<|>))
+import Control.Monad
+-- import Control.Monad.ST
+import Data.Array
+-- import Data.Array.ST
+-- import Data.Bits
+import Data.Char
+-- import Data.Foldable
+-- import Data.Function
+-- import Data.Int
+-- import Data.Ix
+import Data.List
+import Data.Maybe
+-- import Data.Tuple
+-- import Data.Void
+-- import Data.Word
+-- import Debug.Trace
+-- import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString.Char8 as BS
+-- import qualified Data.Graph as G
+-- import qualified Data.Map as Map
+-- import qualified Data.Ratio as R
+-- import qualified Data.Set as S
+-- import qualified Data.Sequence as Seq
+-- import qualified Data.Tree as T
+-- import qualified Data.Vector.Unboxed as UV
+-- import qualified Data.Vector.Unboxed.Mutable as UMV
+-- import qualified Numeric as N
+
+tup :: [a] -> (a, a)
+tup [x, y] = (x, y)
+
+list :: (a, a) -> [a]
+list (x, y) = [x, y]
+
+readInt :: IO Int
+readInt = fst . fromJust . BS.readInt <$> BS.getLine
+
+toIntList :: BS.ByteString -> [Int]
+toIntList = unfoldr (BS.readInt . BS.dropWhile isSpace)
+
+readIntList :: IO [Int]
+readIntList = toIntList <$> BS.getLine
+
+readIntLists :: Int -> IO [[Int]]
+readIntLists n = replicateM n readIntList
+
+readTuples :: Int -> IO [(Int, Int)]
+readTuples n = replicateM n (tup <$> readIntList)
+
+readIntListsAll :: IO [[Int]]
+readIntListsAll = map toIntList . BS.lines <$> BS.getContents
+
+readTuplesAll :: IO [(Int, Int)]
+readTuplesAll = map tup <$> readIntListsAll
+
+modulo :: Int
+modulo = 10^9 + 7
+
+md :: Int -> Int
+md x = x `mod` modulo
+
+madd :: Int -> Int -> Int
+madd x y = (x+y) `mod` modulo
+
+mmul :: Int -> Int -> Int
+mmul x y = (x*y) `mod` modulo
+
+-- https://rosettacode.org/wiki/Modular_inverse
+modInv :: Int -> Int -> Maybe Int
+modInv a m
+  | 1 == g = Just (mkPos i)
+  | otherwise = Nothing
+  where
+    (i, _, g) = gcdExt a m
+    mkPos x
+      | x < 0 = x + m
+      | otherwise = x
+
+-- Extended Euclidean algorithm.
+-- Given non-negative a and b, return x, y and g
+-- such that ax + by = g, where g = gcd(a,b).
+-- Note that x or y may be negative.
+gcdExt :: Int -> Int -> (Int, Int, Int)
+gcdExt a 0 = (1, 0, a)
+gcdExt a b =
+  let (q, r) = a `quotRem` b
+      (s, t, g) = gcdExt b r
+  in (t, s - q * t, g)
+
+solve :: Int -> Int -> Int
+solve w h = foldl' mmul 1 (xs ++ ys)
+  where
+    w' = w - 1
+    h' = h - 1
+    a  = min w' h'
+    xs = [w' + h' - a + 1..w' + h']
+    ys = map (fromJust . flip modInv modulo) $ [1..a]
+
+main :: IO ()
+main = do
+  [w, h] <- readIntList
+  print $ solve w h
