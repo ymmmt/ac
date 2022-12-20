80f5851c1fc4bc483de3722b474fcd66b6b1354a
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2022-12-20 16:10:01 +0900

    add file

---
+++ abc167/e/Main.hs
@@ -0,0 +1,98 @@
+{-# OPTIONS_GHC -O2 #-}
+
+-- {-# LANGUAGE BangPatterns #-}
+-- {-# LANGUAGE DeriveFunctor #-}
+-- {-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE FlexibleInstances #-}
+-- {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- {-# LANGUAGE MultiParamTypeClasses #-}
+-- {-# LANGUAGE StandaloneDeriving #-}
+-- {-# LANGUAGE TemplateHaskell #-}
+-- {-# LANGUAGE TupleSections #-}
+-- {-# LANGUAGE TypeFamilies #-}
+
+-- import Control.Applicative ((<*>), Alternative, empty, (<|>))
+import Control.Monad
+-- import Control.Monad.ST
+import Data.Array
+-- import Data.Array.ST
+-- import Data.Bits
+import Data.Char
+-- import Data.Foldable
+-- import Data.Function
+-- import Data.Int
+-- import Data.Ix
+import Data.List
+import Data.Maybe
+-- import Data.Tuple
+-- import Data.Void
+-- import Data.Word
+import Debug.Trace
+-- import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString.Char8 as BS
+-- import qualified Data.Graph as G
+-- import qualified Data.Map as Map
+-- import qualified Data.Ratio as R
+-- import qualified Data.Set as S
+-- import qualified Data.Sequence as Seq
+-- import qualified Data.Tree as T
+-- import qualified Data.Vector.Unboxed as UV
+-- import qualified Data.Vector.Unboxed.Mutable as UMV
+-- import qualified Numeric as N
+
+toIntList :: BS.ByteString -> [Int]
+toIntList = unfoldr (BS.readInt . BS.dropWhile isSpace)
+
+readIntList :: IO [Int]
+readIntList = toIntList <$> BS.getLine
+
+modulo :: Int
+modulo = 998244353
+
+md :: Int -> Int
+md x = x `mod` modulo
+
+infixl 7 `madd`
+madd :: Int -> Int -> Int
+madd x y = (x+y) `mod` modulo
+
+infixl 7 `mmul`
+mmul :: Int -> Int -> Int
+mmul x y = (x*y) `mod` modulo
+
+-- https://rosettacode.org/wiki/Modular_inverse
+modInv :: Int -> Int -> Maybe Int
+modInv a m
+  | 1 == g = Just (mkPos i)
+  | otherwise = Nothing
+  where
+    (i, _, g) = gcdExt a m
+    mkPos x
+      | x < 0 = x + m
+      | otherwise = x
+
+-- Extended Euclidean algorithm.
+-- Given non-negative a and b, return x, y and g
+-- such that ax + by = g, where g = gcd(a,b).
+-- Note that x or y may be negative.
+gcdExt :: Int -> Int -> (Int, Int, Int)
+gcdExt a 0 = (1, 0, a)
+gcdExt a b =
+  let (q, r) = a `quotRem` b
+      (s, t, g) = gcdExt b r
+  in (t, s - q * t, g)
+
+solve :: Int -> Int -> Int -> Int
+solve n m k = foldl1' madd $ zipWith mmul cs xs
+  where
+    f       = listArray (0, n) $ scanl mmul 1 [1..n]
+    finv    = listArray (0, n) $ map (fromJust . flip modInv modulo) (elems f)
+    denom i = finv!i `mmul` finv!(n-i-1)
+    cs      = map ((f!(n-1) `mmul`) . denom) [0..k]
+    xs      = reverse . take (k+1) . drop (n-k-1) $ iterate ((m-1) `mmul`) m
+
+main :: IO ()
+main = do
+  [n, m, k] <- readIntList
+  print $ solve n m k
+  
