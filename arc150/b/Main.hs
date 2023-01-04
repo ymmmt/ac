ac0f8e37cb4f5dea5ee7b27e0fdcec56acc56b1a
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2023-01-04 11:02:19 +0900

    add file

---
+++ arc150/b/Main.hs
@@ -0,0 +1,71 @@
+{-# OPTIONS_GHC -O2 #-}
+
+-- {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveFunctor #-}
+-- {-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- {-# LANGUAGE MultiParamTypeClasses #-}
+-- {-# LANGUAGE MultiWayIf #-}
+-- {-# LANGUAGE StandaloneDeriving #-}
+-- {-# LANGUAGE TemplateHaskell #-}
+-- {-# LANGUAGE TupleSections #-}
+-- {-# LANGUAGE TypeFamilies #-}
+
+-- import Control.Applicative ((<*>), Alternative, empty, (<|>))
+import Control.Monad
+-- import Control.Monad.ST
+import Control.Monad.State
+import Data.Array
+-- import Data.Array.ST
+import Data.Bits
+import Data.Char
+-- import Data.Foldable
+-- import Data.Function
+-- import Data.Int
+-- import Data.Ix
+import Data.List
+import Data.Maybe
+-- import Data.Tuple
+-- import Data.Tuple.Extra
+-- import Data.Void
+import Data.Word
+import Debug.Trace
+-- import System.Random -- random-1.0.1.1
+-- import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString.Char8 as BS
+-- import qualified Data.Graph as G
+-- import qualified Data.Map as M
+-- import qualified Data.Ratio as R
+-- import qualified Data.Set as S
+-- import qualified Data.Sequence as Seq
+-- import qualified Data.Tree as T
+-- import qualified Data.Vector.Unboxed as UV
+-- import qualified Data.Vector.Unboxed.Mutable as UMV
+-- import qualified Numeric as N
+
+readInt :: IO Int
+readInt = fst . fromJust . BS.readInt <$> BS.getLine
+
+toIntList :: BS.ByteString -> [Int]
+toIntList = unfoldr (BS.readInt . BS.dropWhile isSpace)
+
+readIntList :: IO [Int]
+readIntList = toIntList <$> BS.getLine
+
+ceiling' :: Int -> Int -> Int
+ceiling' x y = ceiling (fromIntegral x / fromIntegral y)
+
+solve :: Int -> Int -> Int
+solve a b = minimum [(k+1) * max 0 (q-a) + k*a - b
+                    | q <- map (ceiling' b) [1..sb] ++ [1..sb]
+                    , let k = ceiling' b q]
+  where
+    sb = ceiling . sqrt $ fromIntegral b
+
+main :: IO ()
+main = do
+  t <- readInt
+  replicateM_ t $ do
+    [a, b] <- readIntList
+    print $ solve a b
