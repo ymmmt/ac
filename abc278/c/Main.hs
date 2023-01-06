5dab6293921c1c087f763422c98d9ba59898b67a
yuichi yamamoto <yuichi.yamamoto0720@gmail.com>
2023-01-06 20:53:51 +0900

    add file

---
+++ abc278/c/Main.hs
@@ -0,0 +1,73 @@
+{-# OPTIONS_GHC -O2 #-}
+
+-- {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveFunctor #-}
+-- {-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- {-# LANGUAGE MultiParamTypeClasses #-}
+-- {-# LANGUAGE MultiWayIf #-}
+-- {-# LANGUAGE StandaloneDeriving #-}
+-- {-# LANGUAGE TemplateHaskell #-}
+-- {-# LANGUAGE TupleSections #-}
+-- {-# LANGUAGE TypeFamilies #-}
+
+-- import Control.Applicative ((<*>), Alternative, empty, (<|>))
+import Control.Monad
+-- import Control.Monad.ST
+import Control.Monad.State
+import Data.Array
+-- import Data.Array.ST
+import Data.Bits
+import Data.Char
+-- import Data.Foldable
+-- import Data.Function
+-- import Data.Int
+-- import Data.Ix
+import Data.List
+import Data.Maybe
+-- import Data.Tuple
+-- import Data.Tuple.Extra
+-- import Data.Void
+import Data.Word
+import Debug.Trace
+-- import System.Random -- random-1.0.1.1
+-- import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString.Char8 as BS
+-- import qualified Data.Graph as G
+-- import qualified Data.Map as M
+-- import qualified Data.Ratio as R
+import qualified Data.Set as S
+-- import qualified Data.Sequence as Seq
+-- import qualified Data.Tree as T
+-- import qualified Data.Vector.Unboxed as UV
+-- import qualified Data.Vector.Unboxed.Mutable as UMV
+-- import qualified Numeric as N
+
+toIntList :: BS.ByteString -> [Int]
+toIntList = unfoldr (BS.readInt . BS.dropWhile isSpace)
+
+readIntList :: IO [Int]
+readIntList = toIntList <$> BS.getLine
+
+readIntLists :: Int -> IO [[Int]]
+readIntLists n = replicateM n readIntList
+
+yn :: Bool -> String
+yn True  = "Yes"
+yn False = "No"
+
+solve :: Int -> [[Int]] -> [String]
+solve n = go S.empty
+  where
+    go _  []             = []
+    go fs ([t, a, b]:qs) = case t of
+                             1 -> go (S.insert (a, b) fs) qs
+                             2 -> go (S.delete (a, b) fs) qs
+                             3 -> yn (S.member (a, b) fs && S.member (b, a) fs):go fs qs
+
+main :: IO ()
+main = do
+  [n, q] <- readIntList
+  qs     <- readIntLists q
+  mapM_ putStrLn $ solve n qs
